7f4e7a5433b126c41c3581d54572d657741d936b
briansu <brianbb.su@gmail.com>
2019-10-17 14:06:48 +0800

    Update vimrc

--- codebook/1_Basic/vimrc.cpp
+++ codebook/1_Basic/vimrc.cpp
@@ -1,17 +1,4 @@
 "This file should be placed at ~/.vimrc"
-set re=1
-set nu
-set ai
-set hls
-set tabstop=4
-set softtabstop=4
-set shiftwidth=4
-set expandtab
-set ruler
-set ic
-set incsearch
-set showcmd
-set history=1000
-set cursorline
-set laststatus=2
+se nu ai hls et ru ic is sc cul re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
 nnoremap <c-l> :nohl<cr>
+inoremap {<ENTER> {}<LEFT><ENTER><ENTER><UP><TAB>
