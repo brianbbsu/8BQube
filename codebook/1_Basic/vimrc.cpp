557b62932538e9a686c66aba3a792c0f461d504b
briansu <brianbb.su@gmail.com>
2019-10-17 14:12:41 +0800

    Update vimrc, again

--- codebook/1_Basic/vimrc.cpp
+++ codebook/1_Basic/vimrc.cpp
@@ -1,4 +1,6 @@
 "This file should be placed at ~/.vimrc"
-se nu ai hls et ru ic is sc cul re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
+se nu ai hls et ru ic is sc cul
+se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
+syntax on
 nnoremap <c-l> :nohl<cr>
 inoremap {<ENTER> {}<LEFT><ENTER><ENTER><UP><TAB>
