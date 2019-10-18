56cce992e13c44187727ab1e4054923ed5069a21
baluteshih <brian369999@gmail.com>
2019-10-18 14:28:21 +0800

    Update vimrc.cpp

--- codebook/1_Basic/vimrc.cpp
+++ codebook/1_Basic/vimrc.cpp
@@ -1,6 +1,7 @@
 "This file should be placed at ~/.vimrc"
 se nu ai hls et ru ic is sc cul
 se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
+hi cursorline cterm=none ctermbg=89
 syntax on
 nnoremap <c-l> :nohl<cr>
 inoremap {<ENTER> {}<LEFT><ENTER><ENTER><UP><TAB>
