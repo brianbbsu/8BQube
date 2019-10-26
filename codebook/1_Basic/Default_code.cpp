046586b33313e9e400c49d7cb5026b155e12d4c2
baluteshih <brian369999@gmail.com>
2019-10-27 00:44:48 +0800

    Update Default_code.cpp

--- codebook/1_Basic/Default_code.cpp
+++ codebook/1_Basic/Default_code.cpp
@@ -5,7 +5,6 @@ using namespace __gnu_pbds;
 typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
-#define FILL(i,n) memset(i,n,sizeof i)
 #define X first
 #define Y second
 #define ET cout << "\n"
@@ -13,16 +12,3 @@ typedef pair<ll,ll> pll;
 #define ALL(v) v.begin(),v.end()
 #define pb push_back
 #define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
-#ifdef bbq
-#define debug(...) {\
-    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
-    _do(__VA_ARGS__);\
-}
-#define DB(a,s,e) {for(int _i=s;_i<e;++_i) cerr << a[_i] << " ";cerr << "\n";}
-template<typename T>void _do(T &&x){cerr<<x<<endl;}
-template<typename T,typename ...S> void _do(T &&x,S &&...t){cerr<<x<<", ";_do(t...);}
-template<typename a,typename b> ostream& operator << (ostream &s,const pair<a,b> &p){return s<<"("<<p.X<<","<<p.Y<<")";}
-#else
-#define debug(...)
-#define DB(a,s,e) 
-#endif
