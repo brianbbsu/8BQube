36f9519bba05a4b94196302a1f4f52f701c5fe6a
baluteshih <brian369999@gmail.com>
2019-10-28 13:00:59 +0800

    Update Manacher.cpp

--- codebook/5_String/Manacher.cpp
+++ codebook/5_String/Manacher.cpp
@@ -2,8 +2,8 @@ int z[MAXN];
 int Manacher(string tmp){
 	string s="&";
 	int l=0,r=0,x,ans;
-	for(char c:tmp) s.pb('%'),s.pb(c);
-	s.pb('%'),ans=0,x=0;
+	for(char c:tmp) s.pb(c),s.pb('%');
+	ans=0,x=0;
 	for(int i=1;i<SZ(s);++i){
 		z[i]=r > i ? min(z[2*l-i],r-i) : 1;
 		while(s[i+z[i]]==s[i-z[i]])++z[i];
