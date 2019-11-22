e76e3e27eab5086328f3520f1e3963cc59cce376
baluteshih <brian369999@gmail.com>
2019-11-22 10:23:52 +0800

    Update Kuhn_Munkres.cpp

--- codebook/4_Flow_Matching/Kuhn_Munkres.cpp
+++ codebook/4_Flow_Matching/Kuhn_Munkres.cpp
@@ -1,51 +1,49 @@
-struct KM{//0-base
-	int n,match[MAXN],vx[MAXN],vy[MAXN];
-	int edge[MAXN][MAXN],lx[MAXN],ly[MAXN],slack[MAXN];
-	void init(int _n){n=_n;
-		for(int i=0;i<n;++i)
-			for(int j=0;j<n;++j)
-				edge[i][j]=0;
-	}
-	void addEdge(int x, int y, int w){ 
-		edge[x][y]=w;
-	}
-	bool DFS(int x){
-		vx[x]=1;
-		for(int y=0;y<n;++y){
-			if(vy[y]) continue;
-			if(lx[x]+ly[y]>edge[x][y])
-				slack[y]=min(slack[y], lx[x]+ly[y]-edge[x][y]);
-			else{
-				vy[y]=1;
-				if(!~match[y]||DFS(match[y]))
-        			return match[y] = x,1;
-			}
-		}
-		return 0;
-	}
-	int solve(){
-		fill(match,match+n,-1);
-		fill(lx,lx+n,-INF),fill(ly,ly+n,0);
-		for(int i=0;i<n;++i)
-			for(int j=0;j<n;++j)
-				lx[i]=max(lx[i],edge[i][j]);
-		for(int i=0;i<n;++i){
-			fill(slack,slack+n,INF);
-			while(1){
-				fill(vx,vx+n,0),fill(vy,vy+n,0);
-				if(DFS(i)) break;
-				int d=INF;
-				for(int j=0;j<n;++j)
-					if(!vy[j]) d=min(d,slack[j]);
-				for(int j=0;j<n;++j){
-					if(vx[j]) lx[j]-=d;
-					if(vy[j]) ly[j]+=d;
-					else slack[j]-=d;
-				}
-			}
-		}
-		int res=0;
-		for(int i=0;i<n;++i) res+=edge[match[i]][i];
-		return res;
-	}
+struct KM{// 0-base
+    int w[MAXN][MAXN],hl[MAXN],hr[MAXN],slk[MAXN],n;
+    int fl[MAXN],fr[MAXN],pre[MAXN],qu[MAXN],ql,qr;
+    bool vl[MAXN],vr[MAXN];
+    void init(int _n){n=_n;
+        for(int i=0;i<n;++i)
+            for(int j=0;j<n;++j)
+                w[i][j]=-INF;
+    }
+    void add_edge(int a,int b,int wei){
+        w[a][b]=wei;
+    }
+    bool Check(int x){
+        if(vl[x]=1,~fl[x]) return vr[qu[qr++]=fl[x]]=1;
+        while(~x) swap(x,fr[fl[x]=pre[x]]);
+        return 0;
+    }
+    void Bfs(int s){
+        fill(slk,slk+n,INF);
+        fill(vl,vl+n,0),fill(vr,vr+n,0);
+        ql=qr=0,qu[qr++]=s,vr[s]=1;
+        while(1){
+            int d;
+            while(ql<qr)
+                for(int x=0,y=qu[ql++];x<n;++x) 
+                    if(!vl[x]&&slk[x]>=(d=hl[x]+hr[y]-w[x][y]))
+                        if(pre[x]=y,d) slk[x]=d;
+                        else if(!Check(x)) return;
+            d=INF;
+            for (int x=0;x<n;++x)
+                if (!vl[x]&&d>slk[x]) d=slk[x];
+            for (int x=0;x<n;++x){
+                if(vl[x]) hl[x]+=d;
+                else slk[x]-=d;
+                if(vr[x]) hr[x]-=d;
+            }
+            for (int x=0;x<n;++x)
+                if(!vl[x]&&!slk[x]&&!Check(x)) return;
+        }
+    }
+    int Solve(){
+        fill(fl,fl+n,-1),fill(fr,fr+n,-1),fill(hr,hr+n, 0);
+        for (int i=0;i<n;++i) hl[i]=*max_element(w[i],w[i]+n);
+        for (int i=0;i<n;++i) Bfs(i);
+        int res=0;
+        for (int i=0;i<n;++i) res += w[i][fl[i]];
+        return res;
+    }
 };
