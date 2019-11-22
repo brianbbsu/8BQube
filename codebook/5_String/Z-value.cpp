7294752500f4f20af8389bb7fbcd7e9e24cbeda9
蘇柏瑄(Brian Su) <brianbb.su@gmail.com>
2019-11-22 15:33:19 +0800

    Update Z-value.cpp

--- codebook/5_String/Z-value.cpp
+++ codebook/5_String/Z-value.cpp
@@ -1,20 +1,10 @@
-int z[100005];
-void Z_value(string s){
-	int L=0,R=0;
-	for(int i=1;i<SZ(s);i++){
-		if(i>R){
-			L=R=i;
-			while(R<SZ(s) && s[R-L]==s[R]) ++R;
-			z[i]=R-L;R--;
-		}
-		else{
-			int k=i-L;
-			if(z[k]<R-i+1) z[i]=z[k];
-			else{
-				L=i;
-				while(R<SZ(s) && s[R-L]==s[R]) ++R;
-				z[i]=R-L,R--;
-			}
-		}
-	}
+const int MAXn = 1e5 + 5;
+int z[MAXn];
+void make_z(string s){
+  int l = 0, r = 0;
+  for(int i = 1;i < s.size();i++){
+    for(z[i] = max(0, min(r - i + 1, z[i - l]));
+	i + z[i] < s.size() && s[i + z[i]] == s[z[i]];z[i]++);
+    if(i + z[i] - 1 > r)l = i, r = i + z[i] - 1;
+  }
 }
