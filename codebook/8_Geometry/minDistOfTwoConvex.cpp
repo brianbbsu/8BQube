7548b0f0561c00f59471c8eb2dfd3fada0d8a907
baluteshih <brian369999@gmail.com>
2019-12-12 21:05:14 +0800

    Add files via upload

---
+++ codebook/8_Geometry/minDistOfTwoConvex.cpp
@@ -0,0 +1,15 @@
+// p, q is convex
+double TwoConvexHullMinDist(Point P[], Point Q[], int n, int m) {
+    int YMinP = 0, YMaxQ = 0;
+    double tmp, ans = 999999999;
+    for (i = 0; i < n; ++i) if(P[i].y < P[YMinP].y) YMinP = i;
+    for (i = 0; i < m; ++i) if(Q[i].y > Q[YMaxQ].y) YMaxQ = i;
+    P[n] = P[0], Q[m] = Q[0];
+    for (int i = 0; i < n; ++i) {
+        while (tmp = Cross(Q[YMaxQ + 1] - P[YMinP + 1], P[YMinP] - P[YMinP + 1]) > Cross(Q[YMaxQ] - P[YMinP + 1], P[YMinP] - P[YMinP + 1])) YMaxQ = (YMaxQ + 1) % m;
+        if (tmp < 0) ans = min(ans, PointToSegDist(P[YMinP], P[YMinP + 1], Q[YMaxQ]));
+        else ans = min(ans, TwoSegMinDist(P[YMinP], P[YMinP + 1], Q[YMaxQ], Q[YMaxQ + 1]));
+        YMinP = (YMinP + 1) % n;
+    }
+    return ans;
+}
