d1750851099e26a59dd21ed3332faa7934c4f653
baluteshih <brian369999@gmail.com>
2020-08-13 02:08:50 +0800

    Update Convexhull3D.cpp

--- codebook/8_Geometry/Convexhull3D.cpp
+++ codebook/8_Geometry/Convexhull3D.cpp
@@ -1,75 +1,81 @@
-#define SIZE(X) (int(X.size()))
-#define PI 3.14159265358979323846264338327950288
-struct Pt{
-	Pt cross(const Pt &p) const 
-	{ return Pt(y * p.z - z * p.y, z * p.x - x * p.z, x * p.y - y * p.x); }
-} info[N];
-int mark[N][N],n, cnt;;
-double mix(const Pt &a, const Pt &b, const Pt &c) 
-{ return a * (b ^ c); }
-double area(int a, int b, int c) 
-{ return norm((info[b] - info[a]) ^ (info[c] - info[a])); }
-double volume(int a, int b, int c, int d) 
-{ return mix(info[b] - info[a], info[c] - info[a], info[d] - info[a]); }
-struct Face{
-	int a, b, c; Face(){}
-	Face(int a, int b, int c): a(a), b(b), c(c) {}
-	int &operator [](int k) 
-	{ if (k == 0) return a; if (k == 1) return b; return c; }
+double eps=1e-12;
+struct P {
+	double x,y,z;
+	P(double x=0,double y=0,double z=0):x(x),y(y),z(z){}
+	P operator+(const P &b)const{return P(x+b.x,y+b.y,z+b.z);}
+	P operator-(const P &b)const{return P(x-b.x,y-b.y,z-b.z);}
+	P operator^(const P &b)const{return P(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);}
+	double operator*(const P&b)const{return x*b.x+y*b.y+z*b.z;}
 };
-vector<Face> face;
-void insert(int a, int b, int c)
-{ face.push_back(Face(a, b, c)); }
-void add(int v) {
-	vector <Face> tmp; int a, b, c; cnt++;
-	for (int i = 0; i < SIZE(face); i++) {
-		a = face[i][0]; b = face[i][1]; c = face[i][2];
-		if(Sign(volume(v, a, b, c)) < 0)
-		mark[a][b] = mark[b][a] = mark[b][c] = mark[c][b] = mark[c][a] = mark[a][c] = cnt;
-		else tmp.push_back(face[i]);
-	} face = tmp;
-	for (int i = 0; i < SIZE(tmp); i++) {
-		a = face[i][0]; b = face[i][1]; c = face[i][2];
-		if (mark[a][b] == cnt) insert(b, a, v);
-		if (mark[b][c] == cnt) insert(c, b, v);
-		if (mark[c][a] == cnt) insert(a, c, v);
-}}
-int Find(){
-	for (int i = 2; i < n; i++) {
-		Pt ndir = (info[0] - info[i]) ^ (info[1] - info[i]);
-		if (ndir == Pt()) continue; swap(info[i], info[2]);
-		for (int j = i + 1; j < n; j++) if (Sign(volume(0, 1, 2, j)) != 0) {
-			swap(info[j], info[3]); insert(0, 1, 2); insert(0, 2, 1); return 1;
-} } return 0; }
-int main() {
-	for (; scanf("%d", &n) == 1; ) { 
-		for (int i = 0; i < n; i++) info[i].Input();
-		sort(info, info + n); n = unique(info, info + n) - info;
-		face.clear(); random_shuffle(info, info + n);
-		if (Find()) { memset(mark, 0, sizeof(mark)); cnt = 0;
-			for (int i = 3; i < n; i++) add(i); vector<Pt> Ndir; 
-			for (int i = 0; i < SIZE(face); ++i) {
-				Pt p = (info[face[i][0]] - info[face[i][1]]) ^
-               (info[face[i][2]] - info[face[i][1]]);
-				p = p / norm( p ); Ndir.push_back(p);
-			} sort(Ndir.begin(), Ndir.end());
-			int ans = unique(Ndir.begin(), Ndir.end()) - Ndir.begin();
-			printf("%d\n", ans);
-		} else printf("1\n"); 
-} }
-double calcDist(const Pt &p, int a, int b, int c) 
-{ return fabs(mix(info[a] - p, info[b] - p, info[c] - p) / area(a, b, c)); }
-//compute the minimal distance of center of any faces
-double findDist() { //compute center of mass
-	double totalWeight = 0; Pt center(.0, .0, .0);
-	Pt first = info[face[0][0]];
-	for (int i = 0; i < SIZE(face); ++i) {
-		Pt p = (info[face[i][0]]+info[face[i][1]]+info[face[i][2]]+first)*.25;
-		double weight = mix(info[face[i][0]] - first, info[face[i][1]] 
-		    - first, info[face[i][2]] - first); 
-		totalWeight += weight; center = center + p * weight;
-	} center = center / totalWeight;
-	double res = 1e100; //compute distance 
-	for (int i = 0; i < SIZE(face); ++i)
-		res = min(res, calcDist(center, face[i][0], face[i][1], face[i][2]));
-    return res; }
+double abs(P a){return sqrt(a*a);}
+double absvol(const P a,const P b,const P c,const P d) {
+    return abs(((b-a)^(c-a))*(d-a))/6;
+}
+struct convex3D {
+static const int maxn=1010;
+struct T{
+    int a,b,c;
+    bool res;
+    T(){}
+    T(int a,int b,int c,bool res=1):a(a),b(b),c(c),res(res){}
+};
+int n,m;
+P p[maxn];
+T f[maxn*8];
+int id[maxn][maxn];
+void init(int _n){n=_n,m=0;}
+bool on(T &t,P &q){return ((p[t.c]-p[t.b])^(p[t.a]-p[t.b]))*(q-p[t.a])>eps;}
+void meow(int q,int a,int b){
+    int g=id[a][b];
+    if(f[g].res){
+        if(on(f[g],p[q]))dfs(q,g);
+        else{
+            id[q][b]=id[a][q]=id[b][a]=m;
+            f[m++]=T(b,a,q,1);
+        }
+    }
+}
+void dfs(int p,int i){
+    f[i].res=0;
+    meow(p,f[i].b,f[i].a);
+    meow(p,f[i].c,f[i].b);
+    meow(p,f[i].a,f[i].c);
+}
+void operator()(){
+    if(n<4)return;
+    if([&](){
+        for(int i=1;i<n;++i)if(abs(p[0]-p[i])>eps)return swap(p[1],p[i]),0;
+        return 1;
+    }() || [&](){
+        for(int i=2;i<n;++i)if(abs((p[0]-p[i])^(p[1]-p[i]))>eps)return swap(p[2],p[i]),0;
+        return 1;
+    }() || [&](){
+        for(int i=3;i<n;++i)if(abs(((p[1]-p[0])^(p[2]-p[0]))*(p[i]-p[0]))>eps)return swap(p[3],p[i]),0;
+        return 1;
+    }())return;
+    for(int i=0;i<4;++i){
+        T t((i+1)%4,(i+2)%4,(i+3)%4,1);
+        if(on(t,p[i]))swap(t.b,t.c);
+        id[t.a][t.b]=id[t.b][t.c]=id[t.c][t.a]=m;
+        f[m++]=t;
+    }
+    for(int i=4;i<n;++i)for(int j=0;j<m;++j)if(f[j].res && on(f[j],p[i])){
+        dfs(i,j);
+        break;
+    }
+    int mm=m; m=0;
+    for(int i=0;i<mm;++i)if(f[i].res)f[m++]=f[i];
+}
+bool same(int i,int j){
+    return !(absvol(p[f[i].a],p[f[i].b],p[f[i].c],p[f[j].a])>eps || absvol(p[f[i].a],p[f[i].b],p[f[i].c],p[f[j].b])>eps || absvol(p[f[i].a],p[f[i].b],p[f[i].c],p[f[j].c])>eps);
+}
+int faces(){
+    int r=0;
+    for(int i=0;i<m;++i){
+        int iden=1;
+        for(int j=0;j<i;++j)if(same(i,j))iden=0;
+        r+=iden;
+    }
+    return r;
+}
+}tb;
