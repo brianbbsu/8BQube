7bede2f0fa46ad68255a3688139b9fe1e21464c2
baluteshih <brian369999@gmail.com>
2020-08-13 02:49:54 +0800

    Update Convexhull3D.cpp

--- codebook/8_Geometry/Convexhull3D.cpp
+++ codebook/8_Geometry/Convexhull3D.cpp
@@ -1,81 +1,121 @@
-double eps=1e-12;
-struct P {
+struct Point{
 	double x,y,z;
-	P(double x=0,double y=0,double z=0):x(x),y(y),z(z){}
-	P operator+(const P &b)const{return P(x+b.x,y+b.y,z+b.z);}
-	P operator-(const P &b)const{return P(x-b.x,y-b.y,z-b.z);}
-	P operator^(const P &b)const{return P(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);}
-	double operator*(const P&b)const{return x*b.x+y*b.y+z*b.z;}
+	Point(double x=0,double y=0,double z=0):x(x),y(y),z(z){}
+	Point operator-(const Point p1){return Point(x-p1.x,y-p1.y,z-p1.z);}
+	Point operator*(Point p){return Point(y*p.z-z*p.y,z*p.x-x*p.z,x*p.y-y*p.x);} 
+	double operator^(Point p){return (x*p.x+y*p.y+z*p.z);}
 };
-double abs(P a){return sqrt(a*a);}
-double absvol(const P a,const P b,const P c,const P d) {
-    return abs(((b-a)^(c-a))*(d-a))/6;
+struct CH3D{
+struct face{int a,b,c;bool ok;}F[8*MAXN];
+int g[MAXN][MAXN],num,n;
+Point P[MAXN];
+double vlen(Point a){return sqrt(a.x*a.x+a.y*a.y+a.z*a.z);}
+Point cross(const Point &a, const Point &b, const Point &c){ 
+	return Point((b.y-a.y)*(c.z-a.z)-(b.z-a.z)*(c.y-a.y),-((b.x-a.x)*(c.z-a.z)
+		-(b.z-a.z)*(c.x-a.x)),(b.x-a.x)*(c.y-a.y)-(b.y-a.y)*(c.x-a.x));
 }
-struct convex3D {
-static const int maxn=1010;
-struct T{
-    int a,b,c;
-    bool res;
-    T(){}
-    T(int a,int b,int c,bool res=1):a(a),b(b),c(c),res(res){}
-};
-int n,m;
-P p[maxn];
-T f[maxn*8];
-int id[maxn][maxn];
-void init(int _n){n=_n,m=0;}
-bool on(T &t,P &q){return ((p[t.c]-p[t.b])^(p[t.a]-p[t.b]))*(q-p[t.a])>eps;}
-void meow(int q,int a,int b){
-    int g=id[a][b];
-    if(f[g].res){
-        if(on(f[g],p[q]))dfs(q,g);
-        else{
-            id[q][b]=id[a][q]=id[b][a]=m;
-            f[m++]=T(b,a,q,1);
-        }
-    }
+double area(Point a,Point b,Point c){return vlen((b-a)*(c-a));}
+double volume(Point a,Point b,Point c,Point d){return (b-a)*(c-a)^(d-a);}
+double dblcmp(Point &p,face &f){return ((P[f.b]-P[f.a])*(P[f.c]-P[f.a]))^(p-P[f.a]);}
+void deal(int p,int a,int b){
+	int f=g[a][b];
+	face add;
+	if(F[f].ok)
+		if(dblcmp(P[p],F[f])>EPS) dfs(p,f);
+		else
+			add.a=b,add.b=a,add.c=p,add.ok=1,
+			g[p][b]=g[a][p]=g[b][a]=num,
+			F[num++]=add;
+} 
+void dfs(int p,int now){
+	F[now].ok=0;
+	deal(p,F[now].b,F[now].a),deal(p,F[now].c,F[now].b),deal(p,F[now].a,F[now].c);
 }
-void dfs(int p,int i){
-    f[i].res=0;
-    meow(p,f[i].b,f[i].a);
-    meow(p,f[i].c,f[i].b);
-    meow(p,f[i].a,f[i].c);
+bool same(int s,int t){
+	Point &a=P[F[s].a];
+	Point &b=P[F[s].b];
+	Point &c=P[F[s].c];
+	return fabs(volume(a,b,c,P[F[t].a]))<EPS && fabs(volume(a,b,c,P[F[t].b]))<EPS
+		&& fabs(volume(a,b,c,P[F[t].c]))<EPS;
 }
-void operator()(){
-    if(n<4)return;
-    if([&](){
-        for(int i=1;i<n;++i)if(abs(p[0]-p[i])>eps)return swap(p[1],p[i]),0;
+void init(int _n){n=_n,num=0;}
+void solve(){
+	face add;
+	bool flag=true;
+	num=0;
+	if(n<4) return;
+	if([&](){
+        for(int i=1;i<n;++i)if(vlen(P[0]-P[i])>EPS)return swap(P[1],P[i]),0;
         return 1;
     }() || [&](){
-        for(int i=2;i<n;++i)if(abs((p[0]-p[i])^(p[1]-p[i]))>eps)return swap(p[2],p[i]),0;
+        for(int i=2;i<n;++i)if(vlen((P[0]-P[i])*(P[1]-P[i]))>EPS)return swap(P[2],P[i]),0;
         return 1;
     }() || [&](){
-        for(int i=3;i<n;++i)if(abs(((p[1]-p[0])^(p[2]-p[0]))*(p[i]-p[0]))>eps)return swap(p[3],p[i]),0;
+        for(int i=3;i<n;++i)if(fabs((P[0]-P[1])*(P[1]-P[2])^(P[0]-P[i]))>EPS)return swap(P[3],P[i]),0;
         return 1;
     }())return;
-    for(int i=0;i<4;++i){
-        T t((i+1)%4,(i+2)%4,(i+3)%4,1);
-        if(on(t,p[i]))swap(t.b,t.c);
-        id[t.a][t.b]=id[t.b][t.c]=id[t.c][t.a]=m;
-        f[m++]=t;
-    }
-    for(int i=4;i<n;++i)for(int j=0;j<m;++j)if(f[j].res && on(f[j],p[i])){
-        dfs(i,j);
-        break;
-    }
-    int mm=m; m=0;
-    for(int i=0;i<mm;++i)if(f[i].res)f[m++]=f[i];
+	for(int i=0;i<4;++i){
+		add.a=(i+1)%4,add.b=(i+2)%4,add.c=(i+3)%4,add.ok=true;
+		if(dblcmp(P[i],add)>0) swap(add.b,add.c);
+		g[add.a][add.b]=g[add.b][add.c]=g[add.c][add.a]=num;
+		F[num++]=add;
+	}
+	for(int i=4;i<n;++i)
+		for(int j=0;j<num;++j)
+			if(F[j].ok && dblcmp(P[i],F[j])>EPS){dfs(i,j);break;}
+	for(int tmp=num,i=(num=0);i<tmp;++i)
+		if(F[i].ok) F[num++]=F[i];
 }
-bool same(int i,int j){
-    return !(absvol(p[f[i].a],p[f[i].b],p[f[i].c],p[f[j].a])>eps || absvol(p[f[i].a],p[f[i].b],p[f[i].c],p[f[j].b])>eps || absvol(p[f[i].a],p[f[i].b],p[f[i].c],p[f[j].c])>eps);
+double area(){
+	double res=0.0;
+	if(n==3)
+		return vlen(cross(P[0],P[1],P[2]))/2.0;
+	for(int i=0;i<num;++i)
+		res+=area(P[F[i].a],P[F[i].b],P[F[i].c]);
+	return res/2.0;
 }
-int faces(){
-    int r=0;
-    for(int i=0;i<m;++i){
-        int iden=1;
-        for(int j=0;j<i;++j)if(same(i,j))iden=0;
-        r+=iden;
-    }
-    return r;
+double volume(){
+	double res=0.0;
+	for(int i=0;i<num;i++)
+		res+=volume(Point(0,0,0),P[F[i].a],P[F[i].b],P[F[i].c]);
+	return fabs(res/6.0);
+} 
+int triangle(){return num;}
+int polygon(){
+	int res=0;
+	for(int i=0,flag=1;i<num;++i,res+=flag,flag=1)
+		for(int j=0;j<i&&flag;++j)
+			flag&=!same(i,j);
+	return res;
 }
-}tb;
+Point getcent(){
+	Point ans(0,0,0),temp=P[F[0].a]; 
+	double v = 0.0,t2; 
+	for(int i=0;i<num;++i)
+		if(F[i].ok == true){
+			Point p1=P[F[i].a],p2=P[F[i].b],p3=P[F[i].c]; 
+			t2 = volume(temp,p1,p2,p3)/6.0;
+			if(t2>0)
+				ans.x += (p1.x+p2.x+p3.x+temp.x)*t2,
+				ans.y += (p1.y+p2.y+p3.y+temp.y)*t2,
+				ans.z += (p1.z+p2.z+p3.z+temp.z)*t2,
+				v += t2; 
+		} 
+	ans.x/=(4*v),ans.y/=(4*v),ans.z/=(4*v); 
+	return ans; 
+} 
+double pointmindis(Point fuck){            
+	double min=99999999; 
+	for(int i=0;i<num;i++)
+		if(F[i].ok==true){ 
+			Point p1=P[F[i].a] , p2=P[F[i].b] , p3=P[F[i].c];                    
+			double a = ( (p2.y-p1.y)*(p3.z-p1.z)-(p2.z-p1.z)*(p3.y-p1.y) ); 
+			double b = ( (p2.z-p1.z)*(p3.x-p1.x)-(p2.x-p1.x)*(p3.z-p1.z) ); 
+			double c = ( (p2.x-p1.x)*(p3.y-p1.y)-(p2.y-p1.y)*(p3.x-p1.x) ); 
+			double d = ( 0-(a*p1.x+b*p1.y+c*p1.z) ); 
+			double temp = fabs(a*fuck.x+b*fuck.y+c*fuck.z+d)/sqrt(a*a+b*b+c*c);                    
+			if(temp<min) min = temp; 
+		} 
+	return min; 
+}
+}hull;
