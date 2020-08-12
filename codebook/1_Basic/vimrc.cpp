90bd15359ec90ba70239f5f7a62bf4d9e446e763
baluteshih <brian369999@gmail.com>
2020-08-13 01:33:18 +0800

    Update vimrc.cpp

--- codebook/1_Basic/vimrc.cpp
+++ codebook/1_Basic/vimrc.cpp
@@ -3,5 +3,5 @@ se nu ai hls et ru ic is sc cul
 se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
 hi cursorline cterm=none ctermbg=89
 syntax on
-nnoremap <c-l> :nohl<cr>
+set background=dark
 inoremap {<ENTER> {}<LEFT><ENTER><ENTER><UP><TAB>
