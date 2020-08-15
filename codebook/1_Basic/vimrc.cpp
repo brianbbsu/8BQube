677c2b0ab883d09a796a0fe5bae1eb60da7bf01b
baluteshih <brian369999@gmail.com>
2020-08-15 12:01:59 +0800

    Update vimrc.cpp

--- codebook/1_Basic/vimrc.cpp
+++ codebook/1_Basic/vimrc.cpp
@@ -1,7 +1,7 @@
 "This file should be placed at ~/.vimrc"
 se nu ai hls et ru ic is sc cul
 se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
-hi cursorline cterm=none ctermbg=89
 syntax on
-set background=dark
+hi cursorline cterm=none ctermbg=89
+set bg=dark
 inoremap {<ENTER> {}<LEFT><ENTER><ENTER><UP><TAB>
