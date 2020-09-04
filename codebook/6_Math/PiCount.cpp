5dbec71b8e8df79d90f4c63a46d56d56656e03a0
i1011 <56438495+i1011@users.noreply.github.com>
2020-09-04 18:29:18 +0800

    Update PiCount.cpp

--- codebook/6_Math/PiCount.cpp
+++ codebook/6_Math/PiCount.cpp
@@ -1,36 +1,37 @@
-int prc[maxn];
-long long phic[msz][nsz];
-
-void sieve() {
-    bitset<maxn> v;
-    pr.push_back(0);
-    for (int i = 2; i < maxn; ++i) {
-        if (!v[i]) pr.push_back(i);
-        for (int j = 1; i * pr[j] < maxn; ++j) {
-            v[i * pr[j]] = true;
-            if (i % pr[j] == 0) break;
+struct PiCount {
+    vector<int> pr;
+    int prc[MAXN];
+    ll phic[MSZ][NSZ];
+    void init() {
+        bitset<MAXN> v;
+        pr.push_back(0);
+        for (int i = 2; i < MAXN; ++i) {
+            if (!v[i]) pr.push_back(i);
+            for (int j = 1; i * pr[j] < MAXN; ++j) {
+                v[i * pr[j]] = true;
+                if (i % pr[j] == 0) break;
+            }
         }
+        for (int i = 1; i < (int)pr.size(); ++i) prc[pr[i]] = 1;
+        for (int i = 1; i < MAXN; ++i) prc[i] += prc[i - 1];
+        memset(phic, -1, sizeof(phic));
     }
-    for (int i = 1; i < pr.size(); ++i) prc[pr[i]] = 1;
-    for (int i = 1; i < maxn; ++i) prc[i] += prc[i - 1];
-}
-long long p2(long long, long long);
-long long phi(long long m, long long n) {
-    if (m < msz && n < nsz && phic[m][n] != -1) return phic[m][n];
-    if (n == 0) return m;
-    if (pr[n] >= m) return 1;
-    long long ret = phi(m, n - 1) - phi(m / pr[n], n - 1);
-    if (m < msz && n < nsz) phic[m][n] = ret;
-    return ret;
-}
-long long pi(long long m) {
-    if (m < maxn) return prc[m];
-    long long n = pi(cbrt(m));
-    return phi(m, n) + n - 1 - p2(m, n);
-}
-long long p2(long long m, long long n) {
-    long long ret = 0;
-    long long lim = sqrt(m);
-    for (int i = n + 1; pr[i] <= lim; ++i) ret += pi(m / pr[i]) - pi(pr[i]) + 1;
-    return ret;
-}
+    ll phi(ll m, ll n) {
+        if (m < MSZ && n < NSZ && phic[m][n] != -1) return phic[m][n];
+        if (n == 0) return m;
+        if (pr[n] >= m) return 1;
+        ll ret = phi(m, n - 1) - phi(m / pr[n], n - 1);
+        if (m < MSZ && n < NSZ) phic[m][n] = ret;
+        return ret;
+    }
+    ll p2(ll m, ll n) {
+        ll ret = 0, lim = sqrt(m);
+        for (int i = n + 1; pr[i] <= lim; ++i) ret += pi(m / pr[i]) - pi(pr[i]) + 1;
+        return ret;
+    }
+    ll pi(ll m) {
+        if (m < MAXN) return prc[m];
+        ll n = pi(cbrt(m));
+        return phi(m, n) + n - 1 - p2(m, n);
+    }
+};
