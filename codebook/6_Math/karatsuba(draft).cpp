d591d503c872e73e43de07fea5ffdc4d58f67c97
i1011 <56438495+i1011@users.noreply.github.com>
2020-10-01 17:52:39 +0800

    Update karatsuba(draft).cpp

--- codebook/6_Math/karatsuba(draft).cpp
+++ codebook/6_Math/karatsuba(draft).cpp
@@ -1,16 +1,26 @@
-struct karatsuba {
+namespace karatsuba {
 #define ADD(x, y) if ((x += y) >= MOD) x -= MOD;
 #define SUB(x, y) if ((x -= y) < 0) x += MOD;
     int mem[MAXN * 4];
-    void mul(const int *x, const int *y, int n, int *ebp, int *esp) {
+    template<int n>
+    void mul(const int *x, const int *y, int *ebp, int *esp) {
         ebp = esp, esp += 2 * n;
-        if (n <= 8) {
+        if (n <= 64) {
+            /*
             fill(ebp, esp, 0);
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < n; ++j) {
                     ebp[i + j] = (ebp[i + j] + (ll)x[i] * y[j]) % MOD;
                 }
             }
+            */
+            ll temp[2 * n] = {};
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    temp[i + j] += (ll)x[i] * y[j];
+                }
+            }
+            for (int i = 0; i < 2 * n; ++i) ebp[i] = temp[i] % MOD;
             return;
         }
         int *q0 = ebp, *q1 = ebp + n / 2, *q2 = ebp + n, *q3 = ebp + n / 2 * 3;
@@ -18,22 +28,29 @@ struct karatsuba {
         copy_n(y + n / 2, n / 2, q3);
         for (int i = 0; i < n / 2; ++i) ADD(q2[i], x[i]);
         for (int i = 0; i < n / 2; ++i) ADD(q3[i], y[i]);
-        mul(q2, q3, n / 2, ebp, esp);
+        mul<n / 2>(q2, q3, ebp, esp);
         copy_n(esp, n, q1);
-
-        mul(x, y, n / 2, ebp, esp);
+ 
+        mul<n / 2>(x, y, ebp, esp);
         copy_n(esp, n / 2, q0);
         for (int i = n / 2; i < n; ++i) ADD(q0[i], esp[i]);
         for (int i = 0; i < n; ++i) SUB(q1[i], esp[i]);
-        mul(x + n / 2, y + n / 2, n / 2, ebp, esp);
+        mul<n / 2>(x + n / 2, y + n / 2, ebp, esp);
         copy_n(esp + n / 2, n / 2, q3);
         for (int i = 0; i < n / 2; ++i) ADD(q2[i], esp[i]); 
         for (int i = 0; i < n; ++i) SUB(q1[i], esp[i]); 
     }
+    template<int n>
+    void resolve(int *x, int *y, int L) {
+        resolve<n / 2>(x, y, L);
+        if (n == L) mul<n>(x, y, mem, mem);
+    }
+    template<>
+    void resolve<0>(int *x, int *y, int L) {
+    }
     void mul(int *x, int n, int *y, int m) {
-        int L = 2 << __lg(max({n, m, 2}) - 1);
+        int L = 2 << __lg(max(n, m) - 1);
         fill(x + n, x + L, 0), fill(y + m, y + L, 0);
-        mul(x, y, L, mem, mem);
+        resolve<MAXN>(x, y, L);
     }
 };
-
