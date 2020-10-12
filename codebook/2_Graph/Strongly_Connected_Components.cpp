8331d76e32037b03d6022aeee0a038ca3748501a
baluteshih <brian369999@gmail.com>
2020-10-12 21:46:46 +0800

    Update Strongly_Connected_Components

--- codebook/2_Graph/Strongly_Connected_Components.cpp
+++ codebook/2_Graph/Strongly_Connected_Components.cpp
@@ -1,37 +1,40 @@
 struct Strongly_CC{//1-base
-	int low[MAXN],vis[MAXN],bln[MAXN],sz[MAXN],n,t,nScc;
-	bitset<MAXN> instack;
+	int low[N], dfn[N], bln[N], sz[N], n, Time, nScc;
+	bitset<N> instack;
 	stack<int> st;
-	vector<int> G[MAXN],SCC[MAXN];
-	void init(int _n){n=_n;
-		for(int i=1;i<=n;++i)
+	vector<int> G[N], SCC[N];
+	void init(int _n) {
+        n = _n;
+		for(int i = 1; i <= n; ++i)
 			G[i].clear();
 	}
-	void add_edge(int a,int b){
+	void add_edge(int a, int b) {
 		G[a].pb(b);
 	}
-	void dfs(int u){
-		vis[u]=low[u]=++t;
-		instack[u]=1,st.push(u);
-		for(int i:G[u])
-			if(!vis[i]) dfs(i),low[u]=min(low[i],low[u]);
-			else if(instack[i]&&vis[i]<vis[u]) 
-				low[u]=min(low[u],vis[i]);
-		if(low[u]==vis[u]){
+	void dfs(int u) {
+		dfn[u] = low[u] = ++Time;
+		instack[u] = 1, st.push(u);
+		for(int i : G[u])
+			if(!dfn[i]) dfs(i), low[u] = min(low[i], low[u]);
+			else if(instack[i] && dfn[i] < dfn[u]) 
+				low[u] = min(low[u], dfn[i]);
+		if(low[u] == dfn[u]) {
 			int tmp;
-			do{
-				tmp=st.top(),st.pop();
-				instack[tmp]=0,bln[tmp]=nScc;
-			}while(tmp!=u);
-			++nScc;
+			do {
+				tmp = st.top(), st.pop();
+				instack[tmp]=0, bln[tmp] = nScc;
+			}while(tmp != u);
+            ++nScc;
 		}
 	}
-	void solve(){
-		FILL(low,0),FILL(vis,0),FILL(bln,0),FILL(sz,0),t=nScc=0;
-		for(int i=1;i<=n;++i) SCC[i].clear();
-		for(int i=1;i<=n;++i)
-			if(!vis[i]) dfs(i);
-		for(int i=1;i<=n;++i){
-			++sz[bln[i]],SCC[bln[i]].pb(i); 
+	void solve() {
+        Time = nScc = 0;
+		for(int i = 1; i <= n; ++i) 
+            SCC[i].clear(), low[i] = dfn[i] = bln[i] = sz[i] = 0;
+		for(int i = 1; i <= n; ++i)
+			if(!dfn[i]) 
+                dfs(i);
+		for(int i = 1; i <= n; ++i)
+			++sz[bln[i]], SCC[bln[i]].pb(i); 
 	}
 };
