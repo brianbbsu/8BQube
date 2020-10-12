ecfb59926571262f0955417b7200caab2d5900fc
baluteshih <brian369999@gmail.com>
2020-10-12 23:45:04 +0800

    Update Virtual_Tree

--- codebook/2_Graph/Virtual_Tree.cpp
+++ codebook/2_Graph/Virtual_Tree.cpp
@@ -1,21 +1,32 @@
-void insert(int x){
-  if(top==-1) 
-    return st[++top]=x,void();
-  int p=LCA(st[top],x);
-  if(p==st[top])
-    return st[++top]=x,void();
-  while(dep[st[top-1]]>dep[p])
-    vG[st[top-1]].pb(st[top]),--top;
-  vG[p].pb(st[top]),--top;
-  if(st[top]!=p) st[++top]=p;
-  st[++top]=x;
+vector<int> vG[N];
+int top, st[N];
+
+void insert(int u) {
+	if(top == -1) 
+		return st[++top] = u, void();
+	int p = LCA(st[top], u);
+	if(p == st[top])
+		return st[++top] = u, void();
+	while(top >= 1 && dep[st[top - 1]] >= dep[p])
+		vG[st[top - 1]].pb(st[top]), --top;
+	if(st[top] != p)
+        vG[p].pb(st[top]), --top, st[++top] = p;
+	st[++top] = u;
 }
-void ending(){
-  while(top>0)
-    vG[st[top-1]].pb(st[top]),--top;
+
+void reset(int u) {
+	for(int i : vG[u])
+		reset(i);
+	vG[u].clear();
 }
-void reset(int x){
-  for(int i:vG[x])
-    reset(i);
-  vG[x].clear();
+
+void solve(vector<int> &v) {
+    top = -1;
+    sort(ALL(v), [&](int a, int b){return dfn[a] < dfn[b];});
+    for (int i : v)
+        insert(i);
+    while (top > 0)
+        vG[st[top - 1]].pb(st[top]), --top;
+    //do something
+    reset(v[0]);
 }
