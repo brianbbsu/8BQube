4080aae11a74fec7f15bb08cefe6f20729b7cb3d
baluteshih <brian369999@gmail.com>
2020-10-12 19:56:56 +0800

    Update black_magic

--- codebook/1_Basic/black_magic.cpp
+++ codebook/1_Basic/black_magic.cpp
@@ -1,9 +1,6 @@
-#include <bits/stdc++.h>
 #include <ext/pb_ds/priority_queue.hpp>
 #include <ext/pb_ds/assoc_container.hpp> //rb_tree
 using namespace __gnu_pbds;
-using namespace std;
-using ll = long long;
 typedef __gnu_pbds::priority_queue<int> heap;
 int main() {
     heap h1, h2;
@@ -11,10 +8,9 @@ int main() {
     h2.push(2), h2.push(4);
     h1.join(h2);
     cout << h1.size() << h2.size() << h1.top() << endl; //404
-    tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> st;
-    tree<ll,ll,less<ll>,rb_tree_tag,tree_order_statistics_node_update> mp;
+    tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> st;
+    tree<ll, ll, less<ll>, rb_tree_tag, tree_order_statistics_node_update> mp;
     for (int x : {0, 2, 3, 4}) st.insert(x);
     cout << *st.find_by_order(2) << st.order_of_key(1) << endl; //31
 }
 //__int128_t,__float128_t
- 
