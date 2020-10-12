94783e42786b9e9a612367c7f59a2026be560971
baluteshih <brian369999@gmail.com>
2020-10-12 20:17:43 +0800

    Update BCC_Vertex

--- codebook/2_Graph/BCC_Vertex.cpp
+++ codebook/2_Graph/BCC_Vertex.cpp
@@ -1,35 +1,51 @@
-vector<int> G[N];// 1-base
-vector<int> bcc[N];
-int low[N],vis[N],Time;
-int bcc_id[N],bcc_cnt;// 1-base
-bool is_cut[N];//whether is av
-int st[N],top;
-void dfs(int u,int pa=-1){
-	int child=0;
-	low[u]=vis[u]=++Time;
-	st[top++]=u;
-	for(int v:G[u])
-		if(!vis[v]){
-			dfs(v,u),++child;
-			low[u]=min(low[u],low[v]);
-			if(vis[u]<=low[v]){
-				is_cut[u]=1;
-				bcc[++bcc_cnt].clear();
-				int t;
-				do{
-					bcc_id[t=st[--top]]=bcc_cnt;
-					bcc[bcc_cnt].push_back(t);
-				}while(t!=v);
-				bcc_id[u]=bcc_cnt;
-				bcc[bcc_cnt].push_back(u);
-			}
-		}
-		else if(vis[v]<vis[u]&&v!=pa)
-			low[u]=min(low[u],vis[v]);
-	if(pa==-1&&child<2)is_cut[u]=0;
+vector<int> G[N]; //1-base
+vector<int> nG[N], bcc[N];
+int low[N], dfn[N], Time;
+int bcc_id[N], bcc_cnt; //1-base
+bool is_cut[N]; //whether is av
+bool cir[N];
+int st[N], top;
+
+void dfs(int u, int pa = -1) {
+    int child = 0;
+    low[u] = dfn[u] = ++Time;
+    st[top++] = u;
+    for(int v : G[u])
+        if(!dfn[v]) {
+            dfs(v,u), ++child;
+            low[u] = min(low[u], low[v]);
+            if(dfn[u] <= low[v]) {
+                is_cut[u]=1;
+                bcc[++bcc_cnt].clear();
+                int t;
+                do {
+                    bcc_id[t = st[--top]] = bcc_cnt;
+                    bcc[bcc_cnt].push_back(t);
+                }while(t != v);
+                bcc_id[u]=bcc_cnt;
+                bcc[bcc_cnt].pb(u);
+            }
+        }
+        else if(dfn[v] < dfn[u] && v!=pa)
+            low[u] = min(low[u], dfn[v]);
+    if(pa == -1 && child < 2)
+        is_cut[u] = 0;
 }
-inline void bcc_init(int n){
-	Time=bcc_cnt=top=0;
-	for(int i=1;i<=n;++i)
-		G[i].clear(),vis[i]=0,is_cut[i]=0,bcc_id[i]=0;
+
+void bcc_init(int n) {
+    Time = bcc_cnt = top = 0;
+    for(int i = 1; i <= n ; ++i)
+        G[i].clear(), dfn[i] = bcc_id[i] = is_cut[i] = 0;
+}
+
+void bcc_solve(int n) {
+    dfs(1);
+    // circle-square tree
+    for(int i = 1; i <= n; ++i)
+        if(is_cut[i])
+            bcc_id[i] = ++bcc_cnt, cir[bcc_cnt] = 1;
+    for(int i = 1; i <= bcc_cnt && !cir[i]; ++i)
+        for(int j : bcc[i])
+            if(is_cut[j])
+                nG[i].pb(bcc_id[j]), nG[bcc_id[j]].pb(i);
 }
