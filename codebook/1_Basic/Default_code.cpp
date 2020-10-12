d01778cdcb8d11adfa5a0fa14e8b5b5af07b0286
baluteshih <brian369999@gmail.com>
2020-10-12 19:53:10 +0800

    Update Default_code

--- codebook/1_Basic/Default_code.cpp
+++ codebook/1_Basic/Default_code.cpp
@@ -1,13 +1,10 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 #define X first
 #define Y second
 #define SZ(a) ((int)a.size())
-#define ALL(v) v.begin(),v.end()
+#define ALL(v) v.begin(), v.end()
 #define pb push_back
-#define IOS() ios_base::sync_with_stdio(0);cin.tie(0);
