e1e1a92e96217b792a1d086eae0478c5142e580c
i1011 <petercheng2000@gmail.com>
2020-10-14 18:53:18 +0800

    Update Fast_Fourier_Transform.cpp Update Fast_Walsh_Transform.cpp

--- codebook/7_Polynomial/Fast_Fourier_Transform.cpp
+++ codebook/7_Polynomial/Fast_Fourier_Transform.cpp
@@ -9,28 +9,7 @@ struct FFT {
       w[i] = val_t(cos(arg), sin(arg));
     }
   }
-  void bitrev(val_t *a, int n) {
-    int i = 0;
-    for (int j = 1; j < n - 1; ++j) {
-      for (int k = n >> 1; (i ^= k) < k; k >>= 1);
-      if (j < i) swap(a[i], a[j]);
-    }
-  }
-  void trans(val_t *a, int n, bool inv = false) {
-    bitrev(a, n);
-    for (int L = 2; L <= n; L <<= 1) {
-      int dx = MAXN / L;
-      for (int i = 0; i < n; i += L) {
-        for (int j = i, x = 0; j < i + (L >> 1); ++j, x += dx) {
-          val_t tmp = a[j + (L >> 1)] * w[x];
-          a[j + (L >> 1)] = a[j] - tmp;
-          a[j] += tmp;
-        }
-      }
-    }
-    if (inv) {
-      reverse(a + 1, a + n);
-      for (int i = 0; i < n; ++i) a[i] /= n;
-    }
-  }
+  void bitrev(val_t *a, int n); // see NTT 
+  void trans(val_t *a, int n, bool inv = false); // see NTT;
+  // remember to replace LL with val_t
 };
