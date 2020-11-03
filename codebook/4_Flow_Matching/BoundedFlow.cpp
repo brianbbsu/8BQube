60f1a4523db55422e30e935e1807f5c764aabd94
baluteshih <brian369999@gmail.com>
2020-11-03 22:56:33 +0800

    Merge Dinic into BoundedFlow

--- codebook/4_Flow_Matching/BoundedFlow.cpp
+++ codebook/4_Flow_Matching/BoundedFlow.cpp
@@ -1,76 +1,75 @@
-struct BoundedFlow{//0-base
-  struct Edge{
-    int to,cap,flow,rev;
-  };
-  vector<Edge> G[MAXN];
-  int n,s,t,dis[MAXN],cur[MAXN],cnt[MAXN];
-  void init(int _n){n=_n;
-    for(int i=0;i<n+2;++i) G[i].clear(),cnt[i]=0;
-  }
-  void add_edge(int u,int v,int lcap,int rcap){
-    cnt[u]-=lcap,cnt[v]+=lcap;
-    G[u].pb(Edge{v,rcap,lcap,(int)G[v].size()});
-    G[v].pb(Edge{u,0,0,(int)G[u].size()-1});
-  }
-  void add_edge(int u,int v,int cap){
-    G[u].pb(Edge{v,cap,0,(int)G[v].size()});
-    G[v].pb(Edge{u,0,0,(int)G[u].size()-1});
-  }
-  int dfs(int u,int cap){
-    if(u==t || !cap) return cap;
-    for(int &i=cur[u];i<(int)G[u].size();++i){
-      Edge &e=G[u][i];
-      if(dis[e.to]==dis[u]+1 && e.flow!=e.cap){
-        int df=dfs(e.to,min(e.cap-e.flow,cap));
-        if(df){
-          e.flow+=df;
-          G[e.to][e.rev].flow-=df;
-          return df;
-        }
-      } 
+struct BoundedFlow {//0-base
+    struct edge {
+        int to, cap, flow, rev;
+    };
+    vector<edge> G[N];
+    int n, s, t, dis[N], cur[N], cnt[N];
+    void init(int _n) {
+        n = _n;
+        for (int i = 0; i < n + 2; ++i)
+            G[i].clear(), cnt[i] = 0;
     }
-    dis[u]=-1;
-    return 0;
-  }
-  bool bfs(){
-    FILL(dis,-1);
-    queue<int> q;
-    q.push(s),dis[s]=0;
-    while(!q.empty()){
-      int tmp=q.front();
-      q.pop();
-      for(auto &u:G[tmp])
-        if(!~dis[u.to] && u.flow!=u.cap){
-          q.push(u.to);
-          dis[u.to]=dis[tmp]+1; 
-        }
-    } 
-    return dis[t]!=-1;
-  }
-  int maxflow(int _s,int _t){
-    s=_s,t=_t;
-    int flow=0,df;
-    while(bfs()){
-      FILL(cur,0);
-      while(df=dfs(s,INF)) flow+=df;
+    void add_edge(int u, int v, int lcap, int rcap) {
+        cnt[u] -= lcap, cnt[v] += lcap;
+        G[u].pb(edge{v, rcap, lcap, SZ(G[v])});
+        G[v].pb(edge{u, 0, 0, SZ(G[u]) - 1});
     }
-    return flow;
-  }
-  bool solve(){
-    int sum=0; 
-    for(int i=0;i<n;++i)
-      if(cnt[i]>0) add_edge(n+1,i,cnt[i]),sum+=cnt[i];
-      else if(cnt[i]<0) add_edge(i,n+2,-cnt[i]);
-    if(sum!=maxflow(n+1,n+2)) sum=-1;
-    for(int i=0;i<n;++i)
-      if(cnt[i]>0) G[n+1].pop_back(),G[i].pop_back();
-      else if(cnt[i]<0) G[i].pop_back(),G[n+2].pop_back();
-    return sum!=-1;
-  }
-  int solve(int _s,int _t){
-    add_edge(_t,_s,INF);
-    if(!solve()) return -1; //invalid flow
-    int x=G[_t].back().flow;
-    return G[_t].pop_back(),G[_s].pop_back(),x;
-  }
+    void add_edge(int u, int v, int cap){
+        G[u].pb(edge{v, cap, 0, SZ(G[v])});
+        G[v].pb(edge{u, 0, 0, SZ(G[u]) - 1});
+    }
+    int dfs(int u, int cap) {
+        if (u == t || !cap) return cap;
+        for (int &i = cur[u]; i < SZ(G[u]); ++i) {
+            edge &e = G[u][i];
+            if (dis[e.to] == dis[u]+1 && e.cap != e.flow) {
+                int df = dfs(e.to, min(e.cap - e.flow, cap));
+                if(df) {
+                    e.flow += df, G[e.to][e.rev].flow -= df;
+                    return df;
+                }
+            }
+        }
+        dis[u] = -1;
+        return 0;
+    }
+    bool bfs() {
+        fill_n(dis, n + 3, -1);
+        queue<int> q;
+        q.push(s), dis[s] = 0;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (edge &e : G[u])
+                if (!~dis[e.to] && e.flow != e.cap)
+                    q.push(e.to), dis[e.to] = dis[u] + 1;
+        }
+        return dis[t] != -1;
+    }
+    int maxflow(int _s, int _t) {
+        s = _s, t = _t;
+        int flow = 0, df;
+        while(bfs()) {
+            fill_n(cur, n + 3, 0);
+            while ((df = dfs(s, INF))) flow += df;
+        }
+        return flow;
+    }
+    bool solve() {
+        int sum = 0;
+        for(int i = 0; i < n; ++i)
+            if(cnt[i] > 0) add_edge(n + 1, i, cnt[i]), sum += cnt[i];
+            else if(cnt[i] < 0) add_edge(i, n + 2, -cnt[i]);
+        if(sum != maxflow(n + 1, n + 2)) sum = -1;
+        for(int i = 0; i < n; ++i)
+            if(cnt[i] > 0) G[n + 1].pop_back(), G[i].pop_back();
+            else if(cnt[i] < 0) G[i].pop_back(), G[n + 2].pop_back();
+        return sum != -1;
+    }
+    int solve(int _s, int _t) {
+        add_edge(_t, _s, INF);
+        if(!solve()) return -1; //invalid flow
+        int x = G[_t].back().flow;
+        return G[_t].pop_back(), G[_s].pop_back(), x;
+    }
 };
