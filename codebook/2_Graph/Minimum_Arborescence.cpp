f3b7161b69ddb73e3f38005339b7bdb401159afe
baluteshih <brian369999@gmail.com>
2020-11-03 21:18:23 +0800

    Update Minimum Arborescence

--- codebook/2_Graph/Minimum_Arborescence.cpp
+++ codebook/2_Graph/Minimum_Arborescence.cpp
@@ -1,53 +1,47 @@
-#define INF 0x3f3f3f3f
-template<typename T>
 struct zhu_liu{//O(VE)
-  static const int MAXN=110;
-  struct edge{
-    int u,v;
-    T w;
-    edge(int u=0,int v=0,T w=0):u(u),v(v),w(w){}
-  };
-  vector<edge>E;// 0-base
-  int pe[MAXN],id[MAXN],vis[MAXN];
-  T in[MAXN];
-  void init(){E.clear();}
-  void add_edge(int u,int v,T w){
-    if(u!=v)E.push_back(edge(u,v,w));
-  }
-  T build(int root,int n){
-    T ans=0;
-    for(;;){
-      for(int u=0;u<n;++u)in[u]=INF;
-      for(size_t i=0;i<E.size();++i)
-        if(E[i].u!=E[i].v&&E[i].w<in[E[i].v])
-          pe[E[i].v]=i,in[E[i].v]=E[i].w;
-      for(int u=0;u<n;++u)//no solution
-        if(u!=root&&in[u]==INF)return -INF;
-      int cntnode=0;
-      memset(id,-1,sizeof(int)*n);
-      memset(vis,-1,sizeof(int)*n);
-      for(int u=0;u<n;++u){
-        if(u!=root)ans+=in[u];
-        int v=u;
-        for(;vis[v]!=u&&id[v]==-1&&v!=root;v=E[pe[v]].u)
-          vis[v]=u;
-        if(v!=root&&id[v]==-1){
-          for(int x=E[pe[v]].u;x!=v;x=E[pe[x]].u)
-            id[x]=cntnode;
-          id[v]=cntnode++;
-        }
-      }
-      if(!cntnode)break;//no cycle
-      for(int u=0;u<n;++u)if(id[u]==-1)id[u]=cntnode++;
-      for(size_t i=0;i<E.size();++i){
-        int v=E[i].v;
-        E[i].u=id[E[i].u];
-        E[i].v=id[E[i].v];
-        if(E[i].u!=E[i].v)E[i].w-=in[v];
-      }
-      n=cntnode;
-      root=id[root];
+    struct edge{
+        int u,v;
+        ll w;
+    };
+    vector<edge> E; //0-base
+    int pe[N], id[N], vis[N];
+    ll in[N];
+    void init() {E.clear();}
+    void add_edge(int u, int v, ll w) {
+        if (u != v) E.pb(edge{u, v, w});
     }
-    return ans;
-  }
+    ll build(int root, int n) {
+        ll ans = 0;
+        for(;;) {
+            fill_n(in, n, INF);
+            for (int i = 0; i < SZ(E); ++i)
+                if (E[i].u != E[i].v && E[i].w < in[E[i].v])
+                    pe[E[i].v] = i, in[E[i].v] = E[i].w;
+            for (int u = 0; u < n; ++u)//no solution
+                if (u != root && in[u] == INF) return -INF;
+            int cntnode = 0;
+            fill_n(id, n, -1), fill_n(vis, n, -1);
+            for (int u = 0; u < n; ++u) {
+                if (u != root) ans += in[u];
+                int v = u;
+                while (vis[v] != u && !~id[v] && v != root)
+                    vis[v] = u, v = E[pe[v]].u;
+                if (v != root && !~id[v]) {
+                    for (int x = E[pe[v]].u; x != v; x = E[pe[x]].u)
+                        id[x] = cntnode;
+                    id[v] = cntnode++;
+                }
+            }
+            if (!cntnode) break;//no cycle
+            for (int u = 0; u < n; ++u)
+                if (!~id[u]) id[u] = cntnode++;
+            for (int i = 0; i < SZ(E); ++i) {
+                int v = E[i].v;
+                E[i].u = id[E[i].u], E[i].v = id[E[i].v];
+                if (E[i].u != E[i].v) E[i].w -= in[v];
+            }
+            n = cntnode, root = id[root];
+        }
+        return ans;
+    }
 };
