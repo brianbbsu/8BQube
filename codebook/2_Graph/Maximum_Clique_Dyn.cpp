299dba3de1f996f95d0593c4fd1b380810462999
briansu <brianbb.su@gmail.com>
2020-11-03 23:13:15 +0800

    Add Max Clique Dyn

---
+++ codebook/2_Graph/Maximum_Clique_Dyn.cpp
@@ -0,0 +1,57 @@
+const int N = 150;
+struct MaxClique {  // Maximum Clique
+  bitset<N> a[N], cs[N];
+  int ans, sol[N], q, cur[N], d[N], n;
+  void init(int _n) {
+    n = _n;
+    for (int i = 0; i < n; i++) a[i].reset();
+  }
+  void addEdge(int u, int v) { a[u][v] = a[v][u] = 1; }
+  void csort(vector<int> &r, vector<int> &c) {
+    int mx = 1, km = max(ans - q + 1, 1), t = 0, m = r.size();
+    cs[1].reset(), cs[2].reset();
+    for (int i = 0; i < m; i++) {
+      int p = r[i], k = 1;
+      while ((cs[k] & a[p]).count()) k++;
+      if (k > mx) mx++, cs[mx + 1].reset();
+      cs[k][p] = 1;
+      if (k < km) r[t++] = p;
+    }
+    c.resize(m);
+    if (t) c[t - 1] = 0;
+    for (int k = km; k <= mx; k++)
+      for (int p = cs[k]._Find_first(); p < N; p = cs[k]._Find_next(p))
+        r[t] = p, c[t] = k, t++;
+  }
+  void dfs(vector<int> &r, vector<int> &c, int l, bitset<N> mask) {
+    while (!r.empty()) {
+      int p = r.back();
+      r.pop_back(), mask[p] = 0;
+      if (q + c.back() <= ans) return;
+      cur[q++] = p;
+      vector<int> nr, nc;
+      bitset<N> nmask = mask & a[p];
+      for (int i : r)
+        if (a[p][i]) nr.push_back(i);
+      if (!nr.empty()) {
+        if (l < 4) {
+          for (int i : nr) d[i] = (a[i] & nmask).count();
+          sort(nr.begin(), nr.end(), [&](int x, int y) { return d[x] > d[y]; });
+        }
+        csort(nr, nc), dfs(nr, nc, l + 1, nmask);
+      } else if (q > ans)
+        ans = q, copy_n(cur, q, sol);
+      c.pop_back(), q--;
+    }
+  }
+  int solve(bitset<N> mask = bitset<N>(string(N, '1'))) {  // vertex mask
+    vector<int> r, c;
+    ans = q = 0;
+    for (int i = 0; i < n; i++)
+      if (mask[i]) r.push_back(i);
+    for (int i = 0; i < n; i++) d[i] = (a[i] & mask).count();
+    sort(r.begin(), r.end(), [&](int i, int j) { return d[i] > d[j]; });
+    csort(r, c), dfs(r, c, 1, mask);
+    return ans;  // sol[0 ~ ans-1]
+  }
+} graph;
