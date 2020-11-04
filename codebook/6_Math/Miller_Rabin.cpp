9684464b974b0503cee44ceb9ed01c4ee755f1c4
baluteshih <brian369999@gmail.com>
2020-11-04 12:32:44 +0800

    Update Miller_Rabin

--- codebook/6_Math/Miller_Rabin.cpp
+++ codebook/6_Math/Miller_Rabin.cpp
@@ -3,14 +3,15 @@
 // n < 3,474,749,660,383  6 : pirmes <= 13
 // n < 2^64             7 : 
 // 2, 325, 9375, 28178, 450775, 9780504, 1795265022
-bool Miller_Rabin(ll a,ll n){
-  if((a=a%n)==0) return 1;
-  if(n&1^1) return n==2;
-  ll tmp=(n-1)/((n-1)&(-n+1)),t=__lg((n-1)&(-n+1)),x=1;
-  for(;tmp;tmp/=2,a=mul(a,a,n))
-    if(tmp&1) x=mul(x,a,n);
-  if(x==1 || x==n-1) return 1;
-  while(--t)
-    if((x=mul(x,x,n))==n-1) return 1;
-  return 0;
+bool Miller_Rabin(ll a, ll n) {
+    if((a = a % n) == 0) return 1;
+    if((n & 1) ^ 1) return n == 2;
+    ll tmp = (n - 1) / ((n - 1) & (1 - n));
+    ll t = __lg(((n - 1) & (1 - n))), x = 1;
+    for(; tmp; tmp >>= 1, a = mul(a, a, n))
+        if(tmp & 1) x = mul(x, a, n);
+    if(x == 1 || x == n - 1) return 1;
+    while(--t)
+        if((x = mul(x, x, n)) == n - 1) return 1;
+    return 0;
 }
