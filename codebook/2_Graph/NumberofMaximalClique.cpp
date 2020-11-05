e6a2d69b96ab521eba1f3b23f234b62cb039caa3
baluteshih <brian369999@gmail.com>
2020-11-05 17:41:56 +0800

    Update NumberofMaximalClique

---
+++ codebook/2_Graph/NumberofMaximalClique.cpp
@@ -0,0 +1,38 @@
+struct BronKerbosch { // 1-base
+    int n, a[N], g[N][N];
+    int S, all[N][N], some[N][N], none[N][N];
+    void init(int _n) {
+        n = _n;
+        for (int i = 1; i <= n; ++i)
+            for (int j = 1; j <= n; ++j)
+                g[i][j] = 0;
+    }
+    void add_edge(int u, int v) {
+        g[u][v] = g[v][u] = 1;
+    }
+    void dfs(int d, int an, int sn, int nn) {
+        if (S > 1000) return; // pruning
+        if (sn == 0 && nn == 0) ++S;
+        int u = some[d][0];
+        for(int i = 0; i < sn; ++i) {
+            int v = some[d][i];
+            if(g[u][v]) continue;
+            int tsn = 0, tnn = 0;
+            copy_n(all[d], an, all[d + 1]);
+            all[d + 1][an] = v;
+            for(int j = 0; j < sn; ++j)
+                if(g[v][some[d][j]]) 
+                    some[d + 1][tsn++] = some[d][j];
+            for(int j = 0; j < nn; ++j) 
+                if(g[v][none[d][j]])
+                    none[d + 1][tnn++] = none[d][j];
+            dfs(d + 1, an + 1, tsn, tnn);
+            some[d][i] = 0, none[d][nn ++] = v;
+        }
+    }
+    int solve() {
+        iota(some[0], some[0] + n, 1);
+        S = 0, dfs(0, 0, n, 0);
+        return S;
+    }
+};
