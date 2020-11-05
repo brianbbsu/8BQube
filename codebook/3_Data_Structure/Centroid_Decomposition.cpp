34956ef69e954644f81874dc2efa48a8809c5ef4
baluteshih <brian369999@gmail.com>
2020-11-05 16:50:44 +0800

    Add Centroid Decomposition

---
+++ codebook/3_Data_Structure/Centroid_Decomposition.cpp
@@ -0,0 +1,64 @@
+struct Cent_Dec { // 1-base
+    vector<pll> G[N];
+    pll info[N]; // store info. of itself
+    pll upinfo[N]; // store info. of climbing up
+    int n, pa[N], layer[N], sz[N], done[N];
+    ll dis[__lg(N) + 1][N];
+    void init(int _n) {
+        n = _n, layer[0] = -1;
+        fill_n(pa + 1, n, 0), fill_n(done + 1, n, 0);
+        for (int i = 1; i <= n; ++i) G[i].clear();
+    }
+    void add_edge(int a, int b, int w) {
+        G[a].pb(pll(b, w)), G[b].pb(pll(a, w));
+    }
+    void get_cent(int u, int f, int &mx, int &c, int num) {
+        int mxsz = 0;
+        sz[u] = 1;
+        for (pll e : G[u])
+            if (!done[e.X] && e.X != f) {
+                get_cent(e.X, u, mx, c, num);
+                sz[u] += sz[e.X], mxsz = max(mxsz, sz[e.X]);
+            }
+        if (mx > max(mxsz, num - sz[u]))
+            mx = max(mxsz, num - sz[u]), c = u;
+    }
+    void dfs(int u, int f, ll d, int org) {
+        // if required, add self info or climbing info
+        dis[layer[org]][u] = d;
+        for (pll e : G[u])
+            if (!done[e.X] && e.X != f)
+                dfs(e.X, u, d + e.Y, org);
+    }
+    int cut(int u, int f, int num) {
+        int mx = 1e9, c = 0, lc;
+        get_cent(u, f, mx, c, num);
+        done[c] = 1, pa[c] = f, layer[c] = layer[f] + 1;
+        for (pll e : G[c])
+            if (!done[e.X]) {
+                if (sz[e.X] > sz[c])
+                    lc = cut(e.X, c, num - sz[c]);
+                else
+                    lc = cut(e.X, c, sz[e.X]);
+                upinfo[lc] = pll(), dfs(e.X, c, e.Y, c);
+            }
+        return done[c] = 0, c;
+    }
+    void build(){cut(1, 0, n);}
+    void modify(int u) {
+        for (int a = u, ly = layer[a]; a; a = pa[a], --ly) {
+            info[a].X += dis[ly][u], ++info[a].Y;
+            if (pa[a])
+                upinfo[a].X += dis[ly - 1][u], ++upinfo[a].Y;
+        }
+    }
+    ll query(int u) {
+        ll rt = 0;
+        for (int a = u, ly = layer[a]; a; a = pa[a], --ly) {
+            rt += info[a].X + info[a].Y * dis[ly][u];
+            if (pa[a])
+                rt -= upinfo[a].X + upinfo[a].Y * dis[ly - 1][u];
+        }
+        return rt;
+    }
+};
