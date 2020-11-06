ca6dffef926f8fd676fdda6f001a1628db7b1e6f
briansu <brianbb.su@gmail.com>
2020-11-06 23:44:52 +0800

    Refactor minimum weight general graph perfect matching

--- codebook/4_Flow_Matching/Minimum_Weight_Matching.cpp
+++ codebook/4_Flow_Matching/Minimum_Weight_Matching.cpp
@@ -1,50 +1,43 @@
-struct Graph{//0-base (Perfect Match) 
-  int n,edge[MAXN][MAXN];
-  int match[MAXN],dis[MAXN],onstk[MAXN];
-  vector<int> stk;
-  void init(int _n){n=_n;
-    for(int i=0;i<n;++i) 
-      for(int j=0;j<n;++j)
-        edge[i][j]=0;
+struct Graph {  // 0-base (Perfect Match), n is even
+  int n, match[N], onstk[N], stk[N], tp;
+  ll edge[N][N], dis[N];
+  void init(int _n) {
+    n = _n, tp = 0;
+    for (int i = 0; i < n; ++i)
+      fill_n(edge[i], n, 0);
   }
-  void add_edge(int u,int v,int w){
-    edge[u][v]=edge[v][u]=w;
-  }
-  bool SPFA(int u){
-    if(onstk[u]) return 1;
-    stk.pb(u),onstk[u]=1;
-    for(int v=0;v<n;++v)
-      if(u!=v&&match[u]!=v&&!onstk[v]){
-        int m=match[v];
-        if(dis[m]>dis[u]-edge[v][m]+edge[u][v]){
-          dis[m]=dis[u]-edge[v][m]+edge[u][v];
-          onstk[v]=1,stk.pb(v);
-          if(SPFA(m)) return 1;
-          stk.pop_back(),onstk[v]=0;
+  void add_edge(int u, int v, ll w) { edge[u][v] = edge[v][u] = w; }
+  bool SPFA(int u) {
+    stk[tp++] = u, onstk[u] = 1;
+    for (int v = 0; v < n; ++v)
+      if (!onstk[v] && match[u] != v) {
+        int m = match[v];
+        if (dis[m] > dis[u] - edge[v][m] + edge[u][v]) {
+          dis[m] = dis[u] - edge[v][m] + edge[u][v];
+          onstk[v] = 1, stk[tp++] = v;
+          if (onstk[m] || SPFA(m)) return 1;
+          --tp, onstk[v] = 0;
         }
       }
-    onstk[u]=0,stk.pop_back();
+    onstk[u] = 0, --tp;
     return 0;
   }
-  int solve(){// find a match 
-    for(int i=0;i<n;i+=2)
-      match[i]=i+1,match[i+1]=i;
-    while(1){
-      int found=0;
-      for(int i=0;i<n;++i) dis[i]=onstk[i]=0;
-      for(int i=0;i<n;++i)
-        if(stk.clear(),!onstk[i]&&SPFA(i))
-          for(found=1;stk.size()>=2;){
-            int u=stk.back();
-            stk.pop_back();
-            int v=stk.back();
-            stk.pop_back();
-            match[u]=v,match[v]=u;
+  ll solve() {  // find a match
+    for (int i = 0; i < n; ++i) match[i] = i ^ 1;
+    while (1) {
+      int found = 0;
+      fill_n(dis, n, 0); fill_n(onstk, n, 0);
+      for (int i = 0; i < n; ++i)
+        if (tp = 0, !onstk[i] && SPFA(i))
+          for (found = 1; tp >= 2;) {
+            int u = stk[--tp];
+            int v = stk[--tp];
+            match[u] = v, match[v] = u;
           }
-      if(!found) break;
+      if (!found) break;
     }
-    int ret=0;
-    for(int i=0;i<n;++i) ret+=edge[i][match[i]];
-    return ret>>1;
+    ll ret = 0;
+    for (int i = 0; i < n; ++i) ret += edge[i][match[i]];
+    return ret >> 1;
   }
 };
