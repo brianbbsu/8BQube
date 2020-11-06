88d0a7332ea13ae0dcbe3904d08345bf676adc7c
i1011 <petercheng2000@gmail.com>
2020-11-07 00:17:28 +0800

    Include DLX.cpp

--- codebook/9_Else/DLX.cpp
+++ codebook/9_Else/DLX.cpp
@@ -1,65 +1,98 @@
-namespace dlx {
-int lt[maxn], rg[maxn], up[maxn], dn[maxn], cl[maxn], rw[maxn], bt[maxn], s[maxn], head, sz, ans;
-void init(int c) {
-    for (int i = 0; i < c; ++i) {
-        up[i] = dn[i] = bt[i] = i;
-        lt[i] = i == 0 ? c : i - 1;
-        rg[i] = i == c - 1 ? c : i + 1;
-        s[i] = 0;
+template<bool Exact>
+struct DLX {
+    int lt[NN], rg[NN], up[NN], dn[NN], cl[NN], rw[NN], bt[NN], s[NN], head, sz, ans;
+    int columns;
+    bool vis[NN];
+    void remove(int c) {
+        if (Exact) lt[rg[c]] = lt[c], rg[lt[c]] = rg[c];
+        for (int i = dn[c]; i != c; i = dn[i]) {
+            if (Exact) {
+                for (int j = rg[i]; j != i; j = rg[j])
+                    up[dn[j]] = up[j], dn[up[j]] = dn[j], --s[cl[j]];
+            } else {
+                lt[rg[i]] = lt[i], rg[lt[i]] = rg[i];
+            }
+        }
     }
-    rg[c] = 0, lt[c] = c - 1;
-    up[c] = dn[c] = -1;
-    head = c, sz = c + 1;
-}
-void insert(int r, const vector<int> &col) {
-    if (col.empty()) return;
-    int f = sz;
-    for (int i = 0; i < (int)col.size(); ++i) {
-        int c = col[i], v = sz++;
-        dn[bt[c]] = v;
-        up[v] = bt[c], bt[c] = v;
-        rg[v] = (i + 1 == (int)col.size() ? f : v + 1);
-        rw[v] = r, cl[v] = c;
-        ++s[c];
-        if (i > 0) lt[v] = v - 1;
+    void restore(int c) {
+        for (int i = up[c]; i != c; i = up[i]) {
+            if (Exact) {
+                for (int j = lt[i]; j != i; j = lt[j])
+                    ++s[cl[j]], up[dn[j]] = j, dn[up[j]] = j;
+            } else {
+                lt[rg[i]] = rg[lt[i]] = i;
+            }
+        }
+        if (Exact) lt[rg[c]] = c, rg[lt[c]] = c;
     }
-    lt[f] = sz - 1;
-}
-void remove(int c) {
-    lt[rg[c]] = lt[c], rg[lt[c]] = rg[c];
-    for (int i = dn[c]; i != c; i = dn[i]) {
-        for (int j = rg[i]; j != i; j = rg[j])
-            up[dn[j]] = up[j], dn[up[j]] = dn[j], --s[cl[j]];
+    void init(int c) {
+        columns = c;
+        for (int i = 0; i < c; ++i) {
+            up[i] = dn[i] = bt[i] = i;
+            lt[i] = i == 0 ? c : i - 1;
+            rg[i] = i == c - 1 ? c : i + 1;
+            s[i] = 0;
+        }
+        rg[c] = 0, lt[c] = c - 1;
+        up[c] = dn[c] = -1;
+        head = c, sz = c + 1;
     }
-}
-void restore(int c) {
-    for (int i = up[c]; i != c; i = up[i]) {
-        for (int j = lt[i]; j != i; j = lt[j])
-            ++s[cl[j]], up[dn[j]] = j, dn[up[j]] = j;
+    void insert(int r, const vector<int> &col) {
+        if (col.empty()) return;
+        int f = sz;
+        for (int i = 0; i < (int)col.size(); ++i) {
+            int c = col[i], v = sz++;
+            dn[bt[c]] = v;
+            up[v] = bt[c], bt[c] = v;
+            rg[v] = (i + 1 == (int)col.size() ? f : v + 1);
+            rw[v] = r, cl[v] = c;
+            ++s[c];
+            if (i > 0) lt[v] = v - 1;
+        }
+        lt[f] = sz - 1;
     }
-    lt[rg[c]] = c, rg[lt[c]] = c;
-}
-// Call dlx::make after inserting all rows.
-void make(int c) {
-    for (int i = 0; i < c; ++i)
-        dn[bt[i]] = i, up[i] = bt[i];
-}
-void dfs(int dep) {
-    if (dep >= ans) return;
-    if (rg[head] == head) return ans = dep, void();
-    if (dn[rg[head]] == rg[head]) return;
-    int c = rg[head];
-    int w = c;
-    for (int x = c; x != head; x = rg[x]) if (s[x] < s[w]) w = x;
-    remove(w);
-    for (int i = dn[w]; i != w; i = dn[i]) {
-        for (int j = rg[i]; j != i; j = rg[j]) remove(cl[j]);
-        dfs(dep + 1);
-        for (int j = lt[i]; j != i; j = lt[j]) restore(cl[j]);
+    int h() {
+        int ret = 0;
+        memset(vis, 0, sizeof(bool) * sz);
+        for (int x = rg[head]; x != head; x = rg[x]) {
+            if (vis[x]) continue;
+            vis[x] = true, ++ret;
+            for (int i = dn[x]; i != x; i = dn[i]) {
+                for (int j = rg[i]; j != i; j = rg[j])
+                    vis[cl[j]] = true;
+            }
+        }
+        return ret;
     }
-    restore(w);
-}
-int solve() {
-  ans = 1e9, dfs(0);
-  return ans;
-}}
+    void dfs(int dep) {
+        if (dep + (Exact ? 0 : h()) >= ans) return;
+        if (rg[head] == head) return ans = dep, void();
+        if (dn[rg[head]] == rg[head]) return;
+        int c = rg[head];
+        int w = c;
+        for (int x = c; x != head; x = rg[x]) if (s[x] < s[w]) w = x;
+        if (Exact) {
+            remove(w);
+            for (int i = dn[w]; i != w; i = dn[i]) {
+                for (int j = rg[i]; j != i; j = rg[j]) remove(cl[j]);
+                dfs(dep + 1);
+                for (int j = lt[i]; j != i; j = lt[j]) restore(cl[j]);
+            }
+            restore(w);
+        } else {
+            for (int i = dn[w]; i != w; i = dn[i]) {
+                remove(i);
+                for (int j = rg[i]; j != i; j = rg[j]) remove(j);
+                dfs(dep + 1);
+                for (int j = lt[i]; j != i; j = lt[j]) restore(j);
+                restore(i);
+            }
+        }
+    }
+    int solve() {
+        for (int i = 0; i < columns; ++i)
+            dn[bt[i]] = i, up[i] = bt[i];
+        ans = 1e9, dfs(0);
+        return ans;
+    }
+};
