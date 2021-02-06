c1afd04f078f0471da210c9805ae3dd9e59b5c66
baluteshih <brian369999@gmail.com>
2021-02-06 20:01:14 +0800

    Update FWT

--- codebook/7_Polynomial/Fast_Walsh_Transform.cpp
+++ codebook/7_Polynomial/Fast_Walsh_Transform.cpp
@@ -1,12 +1,30 @@
 /* x: a[j], y: a[j + (L >> 1)]
-or: (y += x), (y -= x) and: (x += y), (x -= y)
-xor: (x+y, x-y), (x+y, x-y)/2               */
-void fwt(val_t *a, int n) { //or
-  for (int L = 2; L <= n; L <<= 1) {
-    for (int i = 0; i < n; i += L) {
-      for (int j = i; j < i + (L >> 1); ++j) {
-        a[j + (L >> 1)] += a[j];
-      }
-    }
-  }
+or: (y += x * op), and: (x += y * op)
+xor: (x, y = (x + y) * op, (x - y) * op)
+invop: or, and, xor = -1, -1, 1/2 */
+void fwt(int *a, int n, int op) { //or
+    for (int L = 2; L <= n; L <<= 1)
+        for (int i = 0; i < n; i += L)
+            for (int j = i; j < i + (L >> 1); ++j)
+                a[j + (L >> 1)] += a[j] * op;
+}
+const int N = 21;
+int f[N][1 << N], g[N][1 << N], h[N][1 << N], ct[1 << N];
+void subset_convolution(int *a, int *b, int *c, int L) {
+    // c_k = \sum_{i | j = k, i & j = 0} a_i * b_j
+    int n = 1 << L;
+    for (int i = 1; i < n; ++i)
+        ct[i] = ct[i & (i - 1)] + 1;
+    for (int i = 0; i < n; ++i)
+        f[ct[i]][i] = a[i], g[ct[i]][i] = b[i];
+    for (int i = 0; i <= L; ++i)
+        fwt(f[i], n, 1), fwt(g[i], n, 1);
+    for (int i = 0; i <= L; ++i)
+        for (int j = 0; j <= i; ++j)
+            for (int x = 0; x < n; ++x)
+                h[i][x] += f[j][x] * g[i - j][x];
+    for (int i = 0; i <= L; ++i)
+        fwt(h[i], n, -1);
+    for (int i = 0; i < n; ++i)
+        c[i] = h[ct[i]][i];
 }
