0223c5e23a96f9d486bf187b0d81448df43eb824
i1011 <petercheng2000@gmail.com>
2021-09-02 15:58:15 +0800

    Test format

--- codebook/2_Graph/Dominator_Tree.cpp
+++ codebook/2_Graph/Dominator_Tree.cpp
@@ -1,55 +1,48 @@
-struct dominator_tree{//1-base
-  vector<int> G[N],rG[N];
+struct dominator_tree { // 1-base
+  vector<int> G[N], rG[N];
   int n, pa[N], dfn[N], id[N], Time;
   int semi[N], idom[N], best[N];
-  vector<int> tree[N];//dominator_tree
+  vector<int> tree[N]; // dominator_tree
   void init(int _n) {
     n = _n;
-    for(int i = 1; i <= n; ++i)
-      G[i].clear(), rG[i].clear();
+    for (int i = 1; i <= n; ++i) G[i].clear(), rG[i].clear();
   }
-  void add_edge(int u, int v) {
-    G[u].pb(v), rG[v].pb(u);
-  }
+  void add_edge(int u, int v) { G[u].pb(v), rG[v].pb(u); }
   void dfs(int u) {
     id[dfn[u] = ++Time] = u;
-    for(auto v : G[u])
-      if(!dfn[v])
-        dfs(v), pa[dfn[v]] = dfn[u];
+    for (auto v : G[u])
+      if (!dfn[v]) dfs(v), pa[dfn[v]] = dfn[u];
   }
   int find(int y, int x) {
-    if(y <= x)
-      return y;
+    if (y <= x) return y;
     int tmp = find(pa[y], x);
-    if(semi[best[y]] > semi[best[pa[y]]])
-      best[y] = best[pa[y]];
+    if (semi[best[y]] > semi[best[pa[y]]]) best[y] = best[pa[y]];
     return pa[y] = tmp;
   }
   void tarjan(int root) {
     Time = 0;
-    for(int i = 1; i <= n; ++i){
+    for (int i = 1; i <= n; ++i) {
       dfn[i] = idom[i] = 0;
       tree[i].clear();
       best[i] = semi[i] = i;
     }
     dfs(root);
-    for(int i = Time; i > 1; --i) {
+    for (int i = Time; i > 1; --i) {
       int u = id[i];
-      for(auto v : rG[u]) 
-        if(v = dfn[v]) {
+      for (auto v : rG[u])
+        if (v = dfn[v]) {
           find(v, i);
           semi[i] = min(semi[i], semi[best[v]]);
         }
       tree[semi[i]].pb(i);
-      for(auto v : tree[pa[i]]) {
+      for (auto v : tree[pa[i]]) {
         find(v, pa[i]);
         idom[v] = semi[best[v]] == pa[i] ? pa[i] : best[v];
       }
       tree[pa[i]].clear();
     }
-    for(int i = 2; i <= Time; ++i) {
-      if(idom[i] != semi[i])
-        idom[i] = idom[idom[i]];
+    for (int i = 2; i <= Time; ++i) {
+      if (idom[i] != semi[i]) idom[i] = idom[idom[i]];
       tree[id[idom[i]]].pb(id[i]);
     }
   }
