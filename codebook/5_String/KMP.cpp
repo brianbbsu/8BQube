ac437e2fdc687538c3522d4ee76c1412a7fe661b
i1011 <petercheng2000@gmail.com>
2021-09-08 13:44:10 +0800

    Update codebook/5_String/KMP.cpp

--- codebook/5_String/KMP.cpp
+++ codebook/5_String/KMP.cpp
@@ -2,14 +2,13 @@ int F[MAXN];
 vector<int> match(string A, string B) {
   vector<int> ans;
   F[0] = -1, F[1] = 0;
-  for (int i = 1, j = 0; i < B.size(); F[++i] = ++j) {
+  for (int i = 1, j = 0; i < SZ(B); F[++i] = ++j) {
     if (B[i] == B[j]) F[i] = F[j]; // optimize
     while (j != -1 && B[i] != B[j]) j = F[j];
   }
-  for (int i = 0, j = 0; i - j + B.size() <= A.size();
-       ++i, ++j) {
+  for (int i = 0, j = 0; i < SZ(A); ++i) {
     while (j != -1 && A[i] != B[j]) j = F[j];
-    if (j == B.size() - 1) ans.pb(i - j);
+    if (++j == SZ(B)) ans.pb(i + 1 - j), j = F[j];
   }
   return ans;
 }
