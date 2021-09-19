7d7a79c76cad0466214313ad741dd40c85636452
i1011 <56438495+i1011@users.noreply.github.com>
2021-09-19 23:29:36 +0800

    Create AdaptiveSimpson.cpp

---
+++ codebook/9_Else/AdaptiveSimpson.cpp
@@ -0,0 +1,17 @@
+using F_t = function<double(double)>;
+pdd simpson(const F_t &f, double l, double r, double fl, double fr, double fm = INFINITY) {
+    if (isinf(fm)) fm = f((l + r) / 2);
+    return {fm, (r - l) / 6 * (fl + 4 * fm + fr)};
+}
+double simpson_ada(const F_t &f, double l, double r, double fl, double fm, double fr, double eps) {
+    double m = (l + r) / 2, s = simpson(f, l, r, fl, fr, fm).second;
+    auto [flm, sl] = simpson(f, l, m, fl, fm);
+    auto [fmr, sr] = simpson(f, m, r, fm, fr);
+    double delta = sl + sr - s;
+    if (abs(delta) <= 15 * eps)
+        return sl + sr + delta / 15;
+    return simpson_ada(f, l, m, fl, flm, fm, eps / 2) + simpson_ada(f, m, r, fm, fmr, fr, eps / 2);
+}
+double simpson_ada(const F_t &f, double l, double r) {
+    return simpson_ada(f, l, r, f(l), f((l + r) / 2), f(r), 1e-9);
+}
