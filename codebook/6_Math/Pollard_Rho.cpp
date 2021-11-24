ba84bf28485dafd32c17c347a8ec54a31a522b55
baluteshih <brian369999@gmail.com>
2021-11-24 19:30:00 +0800

    Update Pollard_Rho.cpp

--- codebook/6_Math/Pollard_Rho.cpp
+++ codebook/6_Math/Pollard_Rho.cpp
@@ -1,12 +1,18 @@
-// does not work when n is prime
-ll f(ll x,ll mod){ return add(mul(x,x,mod),1,mod); }
-ll pollard_rho(ll n){
-  if(!(n&1)) return 2;
-  while(1){
-    ll y=2,x=rand()%(n-1)+1,res=1;
-    for(int sz=2;res==1;y=x,sz*=2)
-      for(int i=0;i<sz&&res<=1;++i)
-        x=f(x,n),res=__gcd(abs(x-y),n);
-    if(res!=0&&res!=n) return res;
+map<ll, int> cnt;
+void PollardRho(ll n) {
+  if (n == 1) return;
+  if (prime(n)) return ++cnt[n], void();        
+  if (n % 2 == 0) return PollardRho(n / 2), ++cnt[2], void();
+  ll x = 2, y = 2, d = 1, p = 1;
+  #define f(x, n, p) ((mul(x, x, n) + p) % n)
+  while (true) {
+    if (d != n && d != 1) {
+      PollardRho(n / d);
+      PollardRho(d);
+      return;
+    }
+    if (d == n) ++p;
+    x = f(x, n, p); y = f(f(y, n, p), n, p);
+    d = __gcd(abs(x - y), n);
   }
 }
