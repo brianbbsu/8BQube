71015b35a2df5febfae2b4d03d274d4b85c8bfae
baluteshih <brian369999@gmail.com>
2021-11-24 11:19:42 +0800

    Update Z-value.cpp

--- codebook/5_String/Z-value.cpp
+++ codebook/5_String/Z-value.cpp
@@ -1,11 +1,10 @@
-const int MAXn = 1e5 + 5;
 int z[MAXn];
-void make_z(string s) {
+void make_z(const string &s) {
   int l = 0, r = 0;
-  for (int i = 1; i < s.size(); i++) {
+  for (int i = 1; i < SZ(s); ++i) {
     for (z[i] = max(0, min(r - i + 1, z[i - l]));
-         i + z[i] < s.size() && s[i + z[i]] == s[z[i]];
-         z[i]++)
+         i + z[i] < SZ(s) && s[i + z[i]] == s[z[i]];
+         ++z[i])
       ;
     if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
   }
