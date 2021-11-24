1615007cb5e2d03767aa2ecf0a43f781dbac7195
baluteshih <brian369999@gmail.com>
2021-11-24 11:28:40 +0800

    Update NTT

--- codebook/7_Polynomial/Number_Theory_Transform.cpp
+++ codebook/7_Polynomial/Number_Theory_Transform.cpp
@@ -2,30 +2,30 @@
 //7*17*(2^23)+1, 998244353, 3
 //1255*(2^20)+1, 1315962881, 3
 //51*(2^25)+1, 1711276033, 29
-template<int MAXN, LL P, LL RT> //MAXN must be 2^k
+template<int MAXN, ll P, ll RT> //MAXN must be 2^k
 struct NTT {
-  LL w[MAXN];
-  LL mpow(LL a, LL n);
-  LL minv(LL a) { return mpow(a, P - 2); }
+  ll w[MAXN];
+  ll mpow(ll a, ll n);
+  ll minv(ll a) { return mpow(a, P - 2); }
   NTT() {
-    LL dw = mpow(RT, (P - 1) / MAXN);
+    ll dw = mpow(RT, (P - 1) / MAXN);
     w[0] = 1;
     for (int i = 1; i < MAXN; ++i) w[i] = w[i - 1] * dw % P;
   }
-  void bitrev(LL *a, int n) {
+  void bitrev(ll *a, int n) {
     int i = 0;
     for (int j = 1; j < n - 1; ++j) {
       for (int k = n >> 1; (i ^= k) < k; k >>= 1);
       if (j < i) swap(a[i], a[j]);
     }
   }
-  void operator()(LL *a, int n, bool inv = false) { //0 <= a[i] < P
+  void operator()(ll *a, int n, bool inv = false) { //0 <= a[i] < P
     bitrev(a, n);
     for (int L = 2; L <= n; L <<= 1) {
       int dx = MAXN / L, dl = L >> 1;
       for (int i = 0; i < n; i += L) {
         for (int j = i, x = 0; j < i + dl; ++j, x += dx) {
-          LL tmp = a[j + dl] * w[x] % P;
+          ll tmp = a[j + dl] * w[x] % P;
           if ((a[j + dl] = a[j] - tmp) < 0) a[j + dl] += P;
           if ((a[j] += tmp) >= P) a[j] -= P;
         }
@@ -33,7 +33,7 @@ struct NTT {
     }
     if (inv) {
       reverse(a + 1, a + n);
-      LL invn = minv(n);
+      ll invn = minv(n);
       for (int i = 0; i < n; ++i) a[i] = a[i] * invn % P;
     }
   }
