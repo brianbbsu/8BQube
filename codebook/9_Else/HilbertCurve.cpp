4ff0859c3affec6c0fe008b3609cee255410fdf1
baluteshih <brian369999@gmail.com>
2021-11-24 20:25:13 +0800

    Add Hilbert Curve

---
+++ codebook/9_Else/HilbertCurve.cpp
@@ -0,0 +1,13 @@
+ll hilbert(int n, int x, int y) {
+  ll res = 0;
+  for (int s = n / 2; s; s >>= 1) {
+    int rx = (x & s) > 0;
+    int ry = (y & s) > 0;
+    res += s * 1ll * s * ((3 * rx) ^ ry);
+    if (ry == 0) {
+      if (rx == 1) x = s - 1 - x, y = s - 1 - y;
+      swap(x, y);
+    }
+  }
+  return res;
+}
