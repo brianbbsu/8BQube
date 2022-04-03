ac1b91d945d3b9db0d6249612ce67e465aced957
baluteshih <brian369999@gmail.com>
2022-04-04 04:28:43 +0800

    Update rotatingSweepLine.cpp

--- codebook/8_Geometry/rotatingSweepLine.cpp
+++ codebook/8_Geometry/rotatingSweepLine.cpp
@@ -1,24 +1,22 @@
 void rotatingSweepLine(vector<pii> &ps) {
-  int n = SZ(ps);
+  int n = SZ(ps), m = 0;
   vector<int> id(n), pos(n);
-  vector<pii> line(n * (n - 1) / 2);
-  int m = 0;
+  vector<pii> line(n * (n - 1));
   for (int i = 0; i < n; ++i)
-    for (int j = i + 1; j < n; ++j)
-      line[m++] = pii(i,j); 
-    sort(ALL(line), [&](const pii &a, const pii &b)->bool {
-      if (ps[a.X].X == ps[a.Y].X)
-        return 0;
-      if (ps[b.X].X == ps[b.Y].X)
-        return 1;
-      return (double)(ps[a.X].Y - ps[a.Y].Y) / (ps[a.X].X - ps[a.Y].X) < (double)(ps[b.X].Y - ps[b.Y].Y) / (ps[b.X].X - ps[b.Y].X);
-  });
-  iota(id, id + n, 0);
-  sort(ALL(id), [&](const int &a,const int &b){ return ps[a] < ps[b]; });
+    for (int j = 0; j < n; ++j)
+      if (i != j) line[m++] = pii(i, j);
+  sort(ALL(line), [&](pii a, pii b) {
+    return cmp(ps[a.Y] - ps[a.X], ps[b.Y] - ps[b.X]);
+  }); // cmp(): polar angle compare
+  iota(ALL(id), 0);
+  sort(ALL(id), [&](int a, int b) {
+    if (ps[a].Y != ps[b].Y) return ps[a].Y < ps[b].Y;
+    return ps[a] < ps[b];
+  }); // initial order, since (1, 0) is the smallest
   for (int i = 0; i < n; ++i) pos[id[i]] = i;
-    for (int i = 0; i < m; ++i) {
-      auto l = line[i];
-      // meow
-      tie(pos[l.X], pos[l.Y], id[pos[l.X]], id[pos[l.Y]]) = make_tuple(pos[l.Y], pos[l.X], l.Y, l.X);
+  for (int i = 0; i < m; ++i) {
+    auto l = line[i];
+    // do something
+    tie(pos[l.X], pos[l.Y], id[pos[l.X]], id[pos[l.Y]]) = make_tuple(pos[l.Y], pos[l.X], l.Y, l.X);
   }
 }
