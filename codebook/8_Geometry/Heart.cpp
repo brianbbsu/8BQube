c73c2b48c22eacb1928669bab8fbd1709aa988dc
baluteshih <brian369999@gmail.com>
2022-05-12 14:34:38 +0800

    Update Heart again (less floating error)

--- codebook/8_Geometry/Heart.cpp
+++ codebook/8_Geometry/Heart.cpp
@@ -1,14 +1,17 @@
-pdd excenter(pdd p0,pdd p1,pdd p2,double &radius){
-  p1=p1-p0,p2=p2-p0;
-  double x1=p1.X,y1=p1.Y,x2=p2.X,y2=p2.Y;   
-  double m=2.*(x1*y2-y1*x2);   
-  center.X=(x1*x1*y2-x2*x2*y1+y1*y2*(y1-y2))/m;   
-  center.Y=(x1*x2*(x2-x1)-y1*y1*x2+x1*y2*y2)/m;   
-  return radius=abs(center),center+p0;
+pdd circenter(pdd p0, pdd p1, pdd p2) { // radius = abs(center)
+  p1 = p1 - p0, p2 = p2 - p0;
+  double x1 = p1.X, y1 = p1.Y, x2 = p2.X, y2 = p2.Y;   
+  double m = 2. * (x1 * y2 - y1 * x2);
+  center.X = (x1 * x1 * y2 - x2 * x2 * y1 + y1 * y2 * (y1 - y2)) / m;
+  center.Y = (x1 * x2 * (x2 - x1) - y1 * y1 * x2 + x1 * y2 * y2) / m;
+  return center + p0;
 }
-pdd incenter(pdd p1,pdd p2,pdd p3,double &radius){
-  double a=abs(p2-p1),b=abs(p3-p1),c=abs(p3-p2);
-  double s=(a+b+c)/2,area=sqrt(s*(s-a)*(s-b)*(s-c));
-  pdd L1=external_bisector(p1,p2,p3),L2=external_bisector(p2,p1,p3);
-  return radius=area/s,intersect(p1,p1+L1,p2,p2+L2),
+pdd incenter(pdd p1, pdd p2, pdd p3) { // radius = area / s * 2
+  double a = abs(p2 - p3), b = abs(p1 - p3), c = abs(p1 - p2);
+  double s = a + b + c;
+  return (a * p1 + b * p2 + c * p3) / s;
 }
+pdd masscenter(pdd p1, pdd p2, pdd p3) 
+{ return (p1 + p2 + p3) / 3; }
+pdd orthcenter(pdd p1, pdd p2, pdd p3) 
+{ return masscenter(p1, p2, p3) * 3 - circenter(p1, p2, p3) * 2; }
