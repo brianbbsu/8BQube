e30f1abe93f5059324fff600891e94bb42613bff
i1011 <56438495+i1011@users.noreply.github.com>
2022-06-02 23:57:42 +0800

    Update minDistOfTwoConvex.cpp

--- codebook/8_Geometry/minDistOfTwoConvex.cpp
+++ codebook/8_Geometry/minDistOfTwoConvex.cpp
@@ -2,13 +2,13 @@ double ConvexHullDist(vector<pdd> A, vector<pdd> B) {
     for (auto &p : B) p = {-p.X, -p.Y};
     auto C = Minkowski(A, B);
     if (PointInConvex(C, pdd(0, 0))) return 0;
-    double ans = 1e9;
+    double ans = kINF;
     for (int i = 0; i < SZ(C); ++i) {
-        ans = min(ans, dot(C[i], C[i]));
+        ans = min(ans, abs2(C[i]));
         const int j = i + 1 == SZ(C) ? 0 : i + 1;
         auto p = foot(C[i], C[j], pdd(0, 0));
         if (!btw(C[i], C[j], p)) continue;
-        ans = min(ans, dot(p, p));
+        ans = min(ans, abs2(p));
     }
     return sqrt(ans);
 }
