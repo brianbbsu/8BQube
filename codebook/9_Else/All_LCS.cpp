f12bd4f394c69d52ed314c8b101cad4e0d83e9e1
baluteshih <brian369999@gmail.com>
2022-07-04 03:16:49 +0800

    Add All_LCS.cpp

---
+++ codebook/9_Else/All_LCS.cpp
@@ -0,0 +1,13 @@
+void all_lcs(string s, string t) { // 0-base
+    vector<int> h(SZ(t));
+    iota(ALL(h), 0);
+    for (int a = 0; a < SZ(s); ++a) {
+        int v = -1;
+        for (int c = 0; c < SZ(t); ++c)
+            if (s[a] == t[c] || h[c] < v)
+                swap(h[c], v);
+        // LCS(s[0, a], t[b, c]) = 
+        // c - b + 1 - sum([h[i] >= b] | i <= c)
+        // h[i] might become -1 !!
+    }
+}
