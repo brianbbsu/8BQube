46cf36b82ad858d27b5067998da96182138a1521
baluteshih <brian369999@gmail.com>
2022-08-19 23:13:32 +0800

    Format Tangent_line_of_two_circles.cpp

--- codebook/8_Geometry/Tangent_line_of_two_circles.cpp
+++ codebook/8_Geometry/Tangent_line_of_two_circles.cpp
@@ -1,22 +1,22 @@
 vector<Line> go( const Cir& c1 , const Cir& c2 , int sign1 ){
   // sign1 = 1 for outer tang, -1 for inter tang
   vector<Line> ret;
-  double d_sq = norm2( c1.O - c2.O );
-  if( d_sq < eps ) return ret;
-  double d = sqrt( d_sq );
-  Pt v = ( c2.O - c1.O ) / d;
-  double c = ( c1.R - sign1 * c2.R ) / d;
-  if( c * c > 1 ) return ret;
-  double h = sqrt( max( 0.0 , 1.0 - c * c ) );
-  for( int sign2 = 1 ; sign2 >= -1 ; sign2 -= 2 ){
-    Pt n = { v.X * c - sign2 * h * v.Y ,
-      v.Y * c + sign2 * h * v.X };
-    Pt p1 = c1.O + n * c1.R;
-    Pt p2 = c2.O + n * ( c2.R * sign1 );
-    if( fabs( p1.X - p2.X ) < eps and
-        fabs( p1.Y - p2.Y ) < eps )
-      p2 = p1 + perp( c2.O - c1.O );
-    ret.push_back( { p1 , p2 } );
+  double d_sq = abs2( c1.O - c2.O );
+  if (sign(d_sq) == 0) return ret;
+  double d = sqrt(d_sq);
+  pdd v = (c2.O - c1.O) / d;
+  double c = (c1.R - sign1 * c2.R) / d;
+  if (c * c > 1) return ret;
+  double h = sqrt(max( 0.0, 1.0 - c * c));
+  for (int sign2 = 1; sign2 >= -1; sign2 -= 2) {
+    pdd n = pdd(v.X * c - sign2 * h * v.Y,
+      v.Y * c + sign2 * h * v.X);
+    pdd p1 = c1.O + n * c1.R;
+    pdd p2 = c2.O + n * (c2.R * sign1);
+    if (sign(p1.X - p2.X) <= 0 and
+        sign(p1.Y - p2.Y) <= 0)
+      p2 = p1 + perp(c2.O - c1.O);
+    ret.pb(Line(p1, p2));
   }
   return ret;
 }
