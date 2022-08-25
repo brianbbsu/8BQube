920ca8b16c19c33300765ca9ffa0426a2f4420df
i1011 <56438495+i1011@users.noreply.github.com>
2022-08-25 15:11:04 +0800

    Update SW-mincut.cpp

--- codebook/4_Flow_Matching/SW-mincut.cpp
+++ codebook/4_Flow_Matching/SW-mincut.cpp
@@ -1,11 +1,9 @@
-// global min cut
-struct SW{ // O(V^3)
+struct SW{ // global min cut, O(V^3)
+    #define REP for (int i = 0; i < n; ++i)
     static const int MXN = 514, INF = 2147483647;
     int vst[MXN], edge[MXN][MXN], wei[MXN];
     void init(int n) {
-        for (int i = 0; i < n; ++i) {
-            fill_n(edge[i], n, 0);
-        }
+        REP fill_n(edge[i], n, 0);
     }
     void addEdge(int u, int v, int w){
         edge[u][v] += w; edge[v][u] += w;
@@ -14,15 +12,12 @@ struct SW{ // O(V^3)
         fill_n(vst, n, 0), fill_n(wei, n, 0);
         s = t = -1;
         int mx, cur;
-        for (int j = 0; j < n; ++j){
+        for (int j = 0; j < n; ++j) {
             mx = -1, cur = 0;
-            for (int i = 0; i < n; ++i) {
-                if (wei[i] > mx) cur = i, mx = wei[i];
-            }
+            REP if (wei[i] > mx) cur = i, mx = wei[i];
             vst[cur] = 1, wei[cur] = -1;
             s = t; t = cur;
-            for (int i = 0; i < n; ++i)
-                if (!vst[i]) wei[i] += edge[cur][i];
+            REP if (!vst[i]) wei[i] += edge[cur][i];
         }
         return mx;
     }
@@ -30,13 +25,12 @@ struct SW{ // O(V^3)
         int res = INF;
         for (int x, y; n > 1; n--){
             res = min(res, search(x, y, n));
-            for (int i = 0; i < n; ++i)
-                edge[i][x] = (edge[x][i] += edge[y][i]);
-            for (int i = 0; i < n; ++i) {
+            REP edge[i][x] = (edge[x][i] += edge[y][i]);
+            REP {
                 edge[y][i] = edge[n - 1][i];
                 edge[i][y] = edge[i][n - 1];
             } // edge[y][y] = 0;
         }
         return res;
     }
-};
+} sw;
