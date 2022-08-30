9ca871127289fd2e793aaf3a7351e5873dbebc64
baluteshih <brian369999@gmail.com>
2022-08-30 13:04:04 +0800

    Update Half_plane_intersection.cpp

--- codebook/8_Geometry/Half_plane_intersection.cpp
+++ codebook/8_Geometry/Half_plane_intersection.cpp
@@ -1,42 +1,39 @@
-bool isin( Line l0, Line l1, Line l2 ){
+bool isin(Line l0, Line l1, Line l2) {
   // Check inter(l1, l2) in l0
-  pdd p = intersect(l1.X,l1.Y,l2.X,l2.Y);
-  return cross(l0.Y - l0.X,p - l0.X) > eps;
+  pdd p = intersect(l1.X, l1.Y, l2.X, l2.Y);
+  return sign(cross(l0.Y - l0.X,p - l0.X)) > 0;
 }
-/* If no solution, check intersect(ret[0], ret[1])
+/* If there is sol., check intersect(ret[0], ret[1])
  * in all the lines. (use (l.Y - l.X) ^ (p - l.X) > 0
  */
 /* --^-- Line.X --^-- Line.Y --^-- */
-vector<Line> halfPlaneInter(vector<Line> lines){
-  int sz = lines.size();
-  vector<double> ata(sz),ord(sz);
-  for(int i=0; i<sz; ++i) {
+vector<Line> halfPlaneInter(vector<Line> lines) {
+  vector<double> ata(SZ(lines)), ord(SZ(lines));
+  for(int i = 0; i < SZ(lines); ++i) {
     ord[i] = i;
     pdd d = lines[i].Y - lines[i].X;
     ata[i] = atan2(d.Y, d.X);
   }
-  sort(ord.begin(), ord.end(), [&](int i,int j){
-      if( fabs(ata[i] - ata[j]) < eps )
-      return (cross(lines[i].Y-lines[i].X, 
-            lines[j].Y-lines[i].X))<0;
-      return ata[i] < ata[j];
-      });
+  sort(ALL(ord), [&](int i, int j) {
+    if (fabs(ata[i] - ata[j]) < eps)
+      return ori(lines[i].X, lines[i].Y, lines[j].Y) < 0; 
+    return ata[i] < ata[j];
+  });
   vector<Line> fin;
-  for (int i=0; i<sz; ++i)
-    if (!i || fabs(ata[ord[i]] - ata[ord[i-1]]) > eps)
+  for (int i = 0; i < SZ(lines); ++i)
+    if (!i || fabs(ata[ord[i]] - ata[ord[i - 1]]) > eps)
       fin.pb(lines[ord[i]]);
   deque<Line> dq;
-  for (int i=0; i<SZ(fin); i++){
-    while(SZ(dq)>=2&&!isin(fin[i],dq[SZ(dq)-2],dq.back())) 
+  for (int i = 0; i < SZ(fin); ++i) {
+    while (SZ(dq) >= 2 && !isin(fin[i], dq[SZ(dq) - 2], dq.back())) 
       dq.pop_back();
-    while(SZ(dq)>=2&&!isin(fin[i],dq[0],dq[1]))
+    while (SZ(dq) >= 2 && !isin(fin[i], dq[0], dq[1]))
       dq.pop_front();
-    dq.push_back(fin[i]);
+    dq.pb(fin[i]);
   }
-  while(SZ(dq)>=3&&!isin(dq[0],dq[SZ(dq)-2],dq.back()))
+  while (SZ(dq) >= 3 && !isin(dq[0], dq[SZ(dq) - 2], dq.back()))
     dq.pop_back();
-  while(SZ(dq)>=3&&!isin(dq.back(), dq[0], dq[1])) 
+  while (SZ(dq) >= 3 && !isin(dq.back(), dq[0], dq[1])) 
     dq.pop_front();
-  vector<Line> res(ALL(dq));
-  return res;
+  return vector<Line>(ALL(dq));
 }
