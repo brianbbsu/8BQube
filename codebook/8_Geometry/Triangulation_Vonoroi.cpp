ee563a8d3bd0968dd973d65e8e031890b5189145
baluteshih <brian369999@gmail.com>
2022-08-30 13:37:11 +0800

    Update Triangulation_Vonoroi.cpp (tinner)

--- codebook/8_Geometry/Triangulation_Vonoroi.cpp
+++ codebook/8_Geometry/Triangulation_Vonoroi.cpp
@@ -1,39 +1,39 @@
 vector<Line> ls[N];
 pll arr[N];
 Line make_line(pdd p, Line l) {
-    pdd d = l.Y - l.X; d = perp(d);
-    pdd m = (l.X + l.Y) / 2;
-    l = Line(m, m + d);
-    if (ori(l.X, l.Y, p) < 0) 
-        l = Line(m + d, m);
-    return l;
+  pdd d = l.Y - l.X; d = perp(d);
+  pdd m = (l.X + l.Y) / 2;
+  l = Line(m, m + d);
+  if (ori(l.X, l.Y, p) < 0) 
+    l = Line(m + d, m);
+  return l;
 }
 double calc_area(int id) {
-    // use to calculate the area of point "strictly in the convex hull"
-    vector<Line> hpi = halfPlaneInter(ls[id]);
-    vector<pdd> ps;
-    for (int i = 0; i < SZ(hpi); ++i)
-        ps.pb(intersect(hpi[i].X, hpi[i].Y, hpi[(i + 1) % SZ(hpi)].X, hpi[(i + 1) % SZ(hpi)].Y));
-    double rt = 0;
-    for (int i = 0; i < SZ(ps); ++i)
-        rt += cross(ps[i], ps[(i + 1) % SZ(ps)]);
-    return fabs(rt) / 2;
+  // use to calculate the area of point "strictly in the convex hull"
+  vector<Line> hpi = halfPlaneInter(ls[id]);
+  vector<pdd> ps;
+  for (int i = 0; i < SZ(hpi); ++i)
+    ps.pb(intersect(hpi[i].X, hpi[i].Y, hpi[(i + 1) % SZ(hpi)].X, hpi[(i + 1) % SZ(hpi)].Y));
+  double rt = 0;
+  for (int i = 0; i < SZ(ps); ++i)
+    rt += cross(ps[i], ps[(i + 1) % SZ(ps)]);
+  return fabs(rt) / 2;
 }
 void solve(int n, pii *oarr) {
-    map<pll, int> mp;
-    for (int i = 0; i < n; ++i)
-        arr[i] = pll(oarr[i].X, oarr[i].Y), mp[arr[i]] = i;
-    build(n, arr); // Triangulation
-    for (auto *t : triang) {
-        vector<int> p;
-        for (int i = 0; i < 3; ++i)
-            if (mp.find(t -> p[i]) != mp.end())
-                p.pb(mp[t -> p[i]]);
-        for (int i = 0; i < SZ(p); ++i)
-            for (int j = i + 1; j < SZ(p); ++j) { 
-                Line l(oarr[p[i]], oarr[p[j]]);
-                ls[p[i]].pb(make_line(oarr[p[i]], l));
-                ls[p[j]].pb(make_line(oarr[p[j]], l));
-            }
-    }
+  map<pll, int> mp;
+  for (int i = 0; i < n; ++i)
+    arr[i] = pll(oarr[i].X, oarr[i].Y), mp[arr[i]] = i;
+  build(n, arr); // Triangulation
+  for (auto *t : triang) {
+    vector<int> p;
+    for (int i = 0; i < 3; ++i)
+      if (mp.find(t->p[i]) != mp.end())
+        p.pb(mp[t->p[i]]);
+    for (int i = 0; i < SZ(p); ++i)
+      for (int j = i + 1; j < SZ(p); ++j) { 
+        Line l(oarr[p[i]], oarr[p[j]]);
+        ls[p[i]].pb(make_line(oarr[p[i]], l));
+        ls[p[j]].pb(make_line(oarr[p[j]], l));
+      }
+  }
 }
