d4d96c9301cb7df489d002fe94a56387d3fe48a8
i1011 <56438495+i1011@users.noreply.github.com>
2022-09-19 17:55:42 +0800

    Update Simplex_Algorithm.cpp

--- codebook/6_Math/Simplex_Algorithm.cpp
+++ codebook/6_Math/Simplex_Algorithm.cpp
@@ -1,31 +1,30 @@
-const int MAXN = 111;
-const int MAXM = 111;
+const int MAXN = 11000, MAXM = 405;
 const double eps = 1E-10;
-double a[MAXN][MAXM], b[MAXN], c[MAXM], d[MAXN][MAXM];
-double x[MAXM];
+double a[MAXN][MAXM], b[MAXN], c[MAXM];
+double d[MAXN][MAXM], x[MAXM];
 int ix[MAXN + MAXM]; // !!! array all indexed from 0
 // max{cx} subject to {Ax<=b,x>=0}
 // n: constraints, m: vars !!!
 // x[] is the optimal solution vector
 // usage : 
 // value = simplex(a, b, c, N, M);
-double simplex(double a[MAXN][MAXM], double b[MAXN],
-    double c[MAXM], int n, int m){
+double simplex(int n, int m){
   ++m;
+  fill_n(d[n], m + 1, 0);
+  fill_n(d[n + 1], m + 1, 0);
+  iota(ix, ix + n + m, 0);
   int r = n, s = m - 1;
-  memset(d, 0, sizeof(d));
-  for (int i = 0; i < n + m; ++i) ix[i] = i;
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m - 1; ++j) d[i][j] = -a[i][j];
     d[i][m - 1] = 1;
     d[i][m] = b[i];
     if (d[r][m] > d[i][m]) r = i;
   }
-  for (int j = 0; j < m - 1; ++j) d[n][j] = c[j];
+  copy_n(c, m - 1, d[n]);
   d[n + 1][m - 1] = -1;
   for (double dd;; ) {
     if (r < n) {
-      int t = ix[s]; ix[s] = ix[r + m]; ix[r + m] = t;
+      swap(ix[s], ix[r + m]);
       d[r][s] = 1.0 / d[r][s];
       for (int j = 0; j <= m; ++j)
         if (j != s) d[r][j] *= -d[r][s];
@@ -35,7 +34,7 @@ double simplex(double a[MAXN][MAXM], double b[MAXN],
         d[i][s] *= d[r][s];
       }
     }
-    r = -1; s = -1;
+    r = s = -1;
     for (int j = 0; j < m; ++j)
       if (s < 0 || ix[s] > ix[j]) {
         if (d[n + 1][j] > eps ||
@@ -53,7 +52,7 @@ double simplex(double a[MAXN][MAXM], double b[MAXN],
   }
   if (d[n + 1][m] < -eps) return -1; // not executable
   double ans = 0;
-  for(int i=0; i<m; i++) x[i] = 0;
+  fill_n(x, m, 0);
   for (int i = m; i < n + m; ++i) { // the missing enumerated x[i] = 0
     if (ix[i] < m - 1){
       ans += d[i - m][m] * c[ix[i]];
