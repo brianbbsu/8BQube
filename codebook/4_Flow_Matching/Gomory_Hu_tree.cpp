71176ae4dded7a7682ee16930e01558b621dab93
baluteshih <brian369999@gmail.com>
2022-09-29 11:32:25 +0800

    Retab (Gomory_Hu_tree.cpp)

--- codebook/4_Flow_Matching/Gomory_Hu_tree.cpp
+++ codebook/4_Flow_Matching/Gomory_Hu_tree.cpp
@@ -1,12 +1,12 @@
 MaxFlow Dinic;
 int g[MAXN]; 
 void GomoryHu(int n) { // 0-base
-    fill_n(g, n, 0);
-    for (int i = 1; i < n; ++i) {
-        Dinic.reset();
-        add_edge(i, g[i], Dinic.maxflow(i, g[i]));
-        for (int j = i + 1; j <= n; ++j)
-            if (g[j] == g[i] && ~Dinic.dis[j])
-                g[j] = i;
-    }
+  fill_n(g, n, 0);
+  for (int i = 1; i < n; ++i) {
+    Dinic.reset();
+    add_edge(i, g[i], Dinic.maxflow(i, g[i]));
+    for (int j = i + 1; j <= n; ++j)
+      if (g[j] == g[i] && ~Dinic.dis[j])
+        g[j] = i;
+  }
 }
