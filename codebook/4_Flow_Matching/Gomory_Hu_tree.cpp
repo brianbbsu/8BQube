a0a61a1c9ca32ebc337b4e82a064a5a8c9b77802
baluteshih <brian369999@gmail.com>
2022-09-29 11:17:21 +0800

    Update Gomory_Hu_tree.cpp

--- codebook/4_Flow_Matching/Gomory_Hu_tree.cpp
+++ codebook/4_Flow_Matching/Gomory_Hu_tree.cpp
@@ -1,28 +1,12 @@
-struct Gomory_Hu_tree { // 0-base
-  MaxFlow Dinic;
-  int n;
-  vector<pii> G[MAXN];
-  void init(int _n) {
-    n = _n;
-    for (int i = 0; i < n; ++i) G[i].clear();
-  }
-  void solve(vector<int> &v) {
-    if (v.size() <= 1) return;
-    int s = rand() % SZ(v);
-    swap(v.back(), v[s]), s = v.back();
-    int t = v[rand() % (SZ(v) - 1)];
-    vector<int> L, R;
-    int x = (Dinic.reset(), Dinic.maxflow(s, t));
-    G[s].pb(pii(t, x)), G[t].pb(pii(s, x));
-    for (int i : v)
-      if (~Dinic.dis[i]) L.pb(i);
-      else R.pb(i);
-    solve(L), solve(R);
-  }
-  void build() {
-    vector<int> v(n);
-    for (int i = 0; i < n; ++i) v[i] = i;
-    solve(v);
-  }
-} ght;
-MaxFlow &Dinic = ght.Dinic;
+MaxFlow Dinic;
+int g[MAXN]; 
+void GomoryHu(int n) { // 0-base
+    fill_n(g, n, 0);
+    for (int i = 1; i < n; ++i) {
+        Dinic.reset();
+        add_edge(i, g[i], Dinic.maxflow(i, g[i]));
+        for (int j = i + 1; j <= n; ++j)
+            if (g[j] == g[i] && ~Dinic.dis[j])
+                g[j] = i;
+    }
+}
