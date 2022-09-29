7bfc11e2aad47aaa2f76f16cb4d767a07db82d54
baluteshih <brian369999@gmail.com>
2022-09-29 11:52:57 +0800

    Update minMaxEnclosingRectangle.cpp

--- codebook/8_Geometry/minMaxEnclosingRectangle.cpp
+++ codebook/8_Geometry/minMaxEnclosingRectangle.cpp
@@ -1,24 +1,24 @@
-pdd solve(vector<pll> &dots){
-  vector<pll> hull;
-  const double INF=1e18,qi=acos(-1)/2*3;
-  cv.dots=dots;
-  hull=cv.hull();
-  double Max=0,Min=INF,deg;
-  ll n=hull.size();
-  hull.pb(hull[0]);
-  for(int i=0,u=1,r=1,l;i<n;++i){
-    pll nw=hull[i+1]-hull[i];
-    while(cross(nw,hull[u+1]-hull[i])>cross(nw,hull[u]-hull[i]))
-      u=(u+1)%n;
-    while(dot(nw,hull[r+1]-hull[i])>dot(nw,hull[r]-hull[i]))
-      r=(r+1)%n;
-    if(!i) l=(r+1)%n;
-    while(dot(nw,hull[l+1]-hull[i])<dot(nw,hull[l]-hull[i]))
-      l=(l+1)%n;
-    Min=min(Min,(double)(dot(nw,hull[r]-hull[i])-dot(nw,hull[l]-hull[i]))*cross(nw,hull[u]-hull[i])/abs2(nw));
-    deg=acos((double)dot(hull[r]-hull[l],hull[u]-hull[i])/abs(hull[r]-hull[l])/abs(hull[u]-hull[i]));
-    deg=(qi-deg)/2;
-    Max=max(Max,(double)abs(hull[r]-hull[l])*abs(hull[u]-hull[i])*sin(deg)*sin(deg));
-  }
-  return pdd(Min,Max);
+const double INF = 1e18, qi = acos(-1) / 2 * 3;
+pdd solve(vector<pll> &dots) {
+#define diff(u, v) (dots[u] - dots[v])
+#define vec(v) (dots[v] - dots[i])
+	hull(dots);
+    double Max = 0, Min = INF, deg;
+    int n = SZ(dots);
+	dots.pb(dots[0]);
+	for (int i = 0, u = 1, r = 1, l = 1; i < n; ++i) {
+		pll nw = vec(i + 1);
+		while (cross(nw, vec(u + 1)) > cross(nw, vec(u)))
+			u = (u + 1) % n;
+		while (dot(nw, vec(r + 1)) > dot(nw, vec(r)))
+			r = (r + 1) % n;
+		if (!i) l = (r + 1) % n;
+		while (dot(nw, vec(l + 1)) < dot(nw, vec(l)))
+			l = (l + 1) % n;
+		Min = min(Min, (double)(dot(nw, vec(r)) - dot(nw, vec(l))) * cross(nw, vec(u)) / abs2(nw));
+		deg = acos(dot(diff(r, l), vec(u)) / abs(diff(r, l)) / abs(vec(u)));
+		deg = (qi - deg) / 2;
+		Max = max(Max, abs(diff(r, l)) * abs(vec(u)) * sin(deg) * sin(deg));
+	}
+	return pdd(Min, Max);
 }
