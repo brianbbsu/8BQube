a08a52d15c333ad75510ea09a2e4fcdfb3724915
baluteshih <brian369999@gmail.com>
2022-09-30 22:06:15 +0800

    Update Fraction.cpp

--- codebook/6_Math/Fraction.cpp
+++ codebook/6_Math/Fraction.cpp
@@ -5,22 +5,17 @@ struct fraction {
     n /= t, d /= t;
     if (d < 0) n = -n, d = -d;
   }
-  fraction operator-() const {
-    return fraction(-n, d);
-  }
-  fraction operator+(const fraction &b) const {
-    return fraction(n * b.d + b.n * d, d * b.d);
-  }
-  fraction operator-(const fraction &b) const {
-    return fraction(n * b.d - b.n * d, d * b.d);
-  }
-  fraction operator*(const fraction &b) const {
-    return fraction(n * b.n, d * b.d);
-  }
-  fraction operator/(const fraction &b) const {
-    return fraction(n * b.d, d * b.n);
-  }
-  void print(){
+  fraction operator-() const 
+  { return fraction(-n, d); }
+  fraction operator+(const fraction &b) const 
+  { return fraction(n * b.d + b.n * d, d * b.d); }
+  fraction operator-(const fraction &b) const 
+  { return fraction(n * b.d - b.n * d, d * b.d); }
+  fraction operator*(const fraction &b) const
+  { return fraction(n * b.n, d * b.d); }
+  fraction operator/(const fraction &b) const 
+  { return fraction(n * b.d, d * b.n); }
+  void print() {
     cout << n;
     if (d != 1) cout << "/" << d;
   }
