b0b2f5447e55b007303c17fe9b87d59d6de7f7d7
baluteshih <brian369999@gmail.com>
2022-09-30 12:30:45 +0800

    Ratab (minMaxEnclosingRectangle.cpp)

--- codebook/8_Geometry/minMaxEnclosingRectangle.cpp
+++ codebook/8_Geometry/minMaxEnclosingRectangle.cpp
@@ -2,23 +2,23 @@ const double INF = 1e18, qi = acos(-1) / 2 * 3;
 pdd solve(vector<pll> &dots) {
 #define diff(u, v) (dots[u] - dots[v])
 #define vec(v) (dots[v] - dots[i])
-	hull(dots);
-    double Max = 0, Min = INF, deg;
-    int n = SZ(dots);
-	dots.pb(dots[0]);
-	for (int i = 0, u = 1, r = 1, l = 1; i < n; ++i) {
-		pll nw = vec(i + 1);
-		while (cross(nw, vec(u + 1)) > cross(nw, vec(u)))
-			u = (u + 1) % n;
-		while (dot(nw, vec(r + 1)) > dot(nw, vec(r)))
-			r = (r + 1) % n;
-		if (!i) l = (r + 1) % n;
-		while (dot(nw, vec(l + 1)) < dot(nw, vec(l)))
-			l = (l + 1) % n;
-		Min = min(Min, (double)(dot(nw, vec(r)) - dot(nw, vec(l))) * cross(nw, vec(u)) / abs2(nw));
-		deg = acos(dot(diff(r, l), vec(u)) / abs(diff(r, l)) / abs(vec(u)));
-		deg = (qi - deg) / 2;
-		Max = max(Max, abs(diff(r, l)) * abs(vec(u)) * sin(deg) * sin(deg));
-	}
-	return pdd(Min, Max);
+  hull(dots);
+  double Max = 0, Min = INF, deg;
+  int n = SZ(dots);
+  dots.pb(dots[0]);
+  for (int i = 0, u = 1, r = 1, l = 1; i < n; ++i) {
+    pll nw = vec(i + 1);
+    while (cross(nw, vec(u + 1)) > cross(nw, vec(u)))
+      u = (u + 1) % n;
+    while (dot(nw, vec(r + 1)) > dot(nw, vec(r)))
+      r = (r + 1) % n;
+    if (!i) l = (r + 1) % n;
+    while (dot(nw, vec(l + 1)) < dot(nw, vec(l)))
+      l = (l + 1) % n;
+    Min = min(Min, (double)(dot(nw, vec(r)) - dot(nw, vec(l))) * cross(nw, vec(u)) / abs2(nw));
+    deg = acos(dot(diff(r, l), vec(u)) / abs(diff(r, l)) / abs(vec(u)));
+    deg = (qi - deg) / 2;
+    Max = max(Max, abs(diff(r, l)) * abs(vec(u)) * sin(deg) * sin(deg));
+  }
+  return pdd(Min, Max);
 }
