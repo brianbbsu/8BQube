909b288df19d894114a50394873bdd1cf600aa09
baluteshih <brian369999@gmail.com>
2022-11-03 15:52:57 +0800

    Add simulated_annealing.cpp

---
+++ codebook/9_Else/simulated_annealing.cpp
@@ -0,0 +1,8 @@
+double factor = 100000;
+const int base = 1e9;
+for (int it = 1; it <= 5000000; ++it) {
+    // ans: answer, nw: current value, rnd(): mt19937 rnd()
+    if (exp(-(nw - ans) / factor) >= (double)(rnd() % base) / base)
+        ans = nw;
+    factor *= 0.99995;
+}
