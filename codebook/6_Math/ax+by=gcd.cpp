5fd0759f9c57fc248d0f5565f6613787c38a3d02
baluteshih <brian369999@gmail.com>
2022-11-04 16:14:35 +0800

    Add tips for ax+by=gcd

--- codebook/6_Math/ax+by=gcd.cpp
+++ codebook/6_Math/ax+by=gcd.cpp
@@ -4,3 +4,8 @@ pll exgcd(ll a, ll b) {
   pll q = exgcd(b, a % b);
   return pll(q.Y, q.X - q.Y * p);
 }
+/* ax+by=res, let x be minimum non-negative
+g, p = gcd(a, b), exgcd(a, b) * res / g
+if p.X < 0: t = (abs(p.X) + b / g - 1) / (b / g)
+else: t = -(p.X / (b / g))
+p += (b / g, -a / g) * t */
