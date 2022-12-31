c82f0285b7200b87973ff8bfe4195589c76d27db
i1011 <56438495+i1011@users.noreply.github.com>
2022-12-31 16:56:56 +0800

    Create misc.py

--- codebook/11_Python/misc.py
+++ codebook/11_Python/misc.py
@@ -2,4 +2,4 @@ from decimal import *
 setcontext(Context(prec=MAX_PREC, Emax=MAX_EMAX, rounding=ROUND_FLOOR))
 print(Decimal(input()) * Decimal(input()))
 from fractions import Fraction
-Fraction('3.1415926535897932').limit_denominator(1000)
+Fraction('3.14159').limit_denominator(10).numerator # 22
