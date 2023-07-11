9bae19420e42ce30db6b33ea559d67ef2468e2a3
baluteshih <brian369999@gmail.com>
2023-07-11 17:31:26 +0800

    Update floor_ceil.cpp

--- codebook/6_Math/floor_ceil.cpp
+++ codebook/6_Math/floor_ceil.cpp
@@ -1,4 +1,4 @@
-int floor(int a,int b) 
-{ return a / b - (a % b && a < 0 ^ b < 0); }
-int ceil(int a,int b)
-{ return a / b + (a % b && a < 0 ^ b > 0); }
+int floor(int a, int b) 
+{ return a / b - (a % b && (a < 0) ^ (b < 0)); }
+int ceil(int a, int b)
+{ return a / b + (a % b && (a < 0) ^ (b > 0)); }
