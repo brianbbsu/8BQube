6f02c006e98991360dbc98712e545e38d1e46939
baluteshih <brian369999@gmail.com>
2023-07-11 17:05:25 +0800

    Fix PointInConvex.cpp

--- codebook/8_Geometry/PointInConvex.cpp
+++ codebook/8_Geometry/PointInConvex.cpp
@@ -2,12 +2,12 @@ bool PointInConvex(const vector<pll> &C, pll p, bool strict = true) {
   int a = 1, b = SZ(C) - 1, r = !strict;
   if (SZ(C) == 0) return false;
   if (SZ(C) < 3) return r && btw(C[0], C.back(), p);
-  if (ori(l[0], l[a], l[b]) > 0) swap(a, b);
-  if (ori(l[0], l[a], p) >= r || ori(l[0], l[b], p) <= -r)
+  if (ori(C[0], C[a], C[b]) > 0) swap(a, b);
+  if (ori(C[0], C[a], p) >= r || ori(C[0], C[b], p) <= -r)
     return false;
   while (abs(a - b) > 1) {
     int c = (a + b) / 2;
-    (ori(l[0], l[c], p) > 0 ? b : a) = c;
+    (ori(C[0], C[c], p) > 0 ? b : a) = c;
   }
-  return ori(l[a], l[b], p) < r;
+  return ori(C[a], C[b], p) < r;
 }
