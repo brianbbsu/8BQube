1bf95c5c77509018dffb03261074b6f204124d82
baluteshih <brian369999@gmail.com>
2023-07-11 17:19:49 +0800

    Add Vector_in_poly.cpp

---
+++ codebook/8_Geometry/Vector_in_poly.cpp
@@ -0,0 +1,10 @@
+// ori(a, b, c) >= 0, valid: "strict" angle from a-b to a-c
+bool btwangle(pll a, pll b, pll c, pll p, int strict) {
+    return ori(a, b, p) >= strict && ori(a, p, c) >= strict;
+}
+// whether vector{cur, p} in counter-clockwise order prv, cur, nxt
+bool inside(pll prv, pll cur, pll nxt, pll p, int strict) {
+    if (ori(cur, nxt, prv) >= 0)
+        return btwangle(cur, nxt, prv, p, strict);
+    return !btwangle(cur, prv, nxt, p, !strict);
+}
