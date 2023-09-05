f2d5c622521a921a9852e7f0f8a5f69da7c5c21f
baluteshih <brian369999@gmail.com>
2023-09-06 03:14:02 +0800

    Update Kuhn_Munkres.cpp

--- codebook/4_Flow_Matching/Kuhn_Munkres.cpp
+++ codebook/4_Flow_Matching/Kuhn_Munkres.cpp
@@ -1,13 +1,13 @@
 struct KM { // 0-base
-  int w[MAXN][MAXN], hl[MAXN], hr[MAXN], slk[MAXN], n;
-  int fl[MAXN], fr[MAXN], pre[MAXN], qu[MAXN], ql, qr;
-  bool vl[MAXN], vr[MAXN];
+  ll w[N][N], hl[N], hr[N], slk[N];
+  int fl[N], fr[N], pre[N], qu[N], ql, qr, n;
+  bool vl[N], vr[N];
   void init(int _n) {
     n = _n;
     for (int i = 0; i < n; ++i)
-      for (int j = 0; j < n; ++j) w[i][j] = -INF;
+      fill_n(w[i], n, -INF);
   }
-  void add_edge(int a, int b, int wei) {
+  void add_edge(int a, int b, ll wei) {
     w[a][b] = wei;
   }
   bool Check(int x) {
@@ -16,18 +16,16 @@ struct KM { // 0-base
     while (~x) swap(x, fr[fl[x] = pre[x]]);
     return 0;
   }
-  void Bfs(int s) {
-    fill(slk, slk + n, INF);
-    fill(vl, vl + n, 0), fill(vr, vr + n, 0);
+  void bfs(int s) {
+    fill_n(slk, n, INF), fill_n(vl, n, 0), fill_n(vr, n, 0);
     ql = qr = 0, qu[qr++] = s, vr[s] = 1;
-    while (1) {
-      int d;
+    for (ll d;;) {
       while (ql < qr)
         for (int x = 0, y = qu[ql++]; x < n; ++x)
-          if (!vl[x] &&
-            slk[x] >= (d = hl[x] + hr[y] - w[x][y]))
+          if (!vl[x] && slk[x] >= (d = hl[x] + hr[y] - w[x][y])) {
             if (pre[x] = y, d) slk[x] = d;
             else if (!Check(x)) return;
+        }
       d = INF;
       for (int x = 0; x < n; ++x)
         if (!vl[x] && d > slk[x]) d = slk[x];
@@ -40,13 +38,12 @@ struct KM { // 0-base
         if (!vl[x] && !slk[x] && !Check(x)) return;
     }
   }
-  int Solve() {
-    fill(fl, fl + n, -1), fill(fr, fr + n, -1),
-      fill(hr, hr + n, 0);
+  ll solve() {
+    fill_n(fl, n, -1), fill_n(fr, n, -1), fill_n(hr, n, 0);
     for (int i = 0; i < n; ++i)
       hl[i] = *max_element(w[i], w[i] + n);
-    for (int i = 0; i < n; ++i) Bfs(i);
-    int res = 0;
+    for (int i = 0; i < n; ++i) bfs(i);
+    ll res = 0;
     for (int i = 0; i < n; ++i) res += w[i][fl[i]];
     return res;
   }
