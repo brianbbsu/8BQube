fe9770daf7487705a6b445ab68b06f2daae82736
baluteshih <brian369999@gmail.com>
2023-10-07 03:25:28 +0800

    Update MinimumSteinerTree.cpp

--- codebook/2_Graph/MinimumSteinerTree.cpp
+++ codebook/2_Graph/MinimumSteinerTree.cpp
@@ -1,56 +1,47 @@
-// Minimum Steiner Tree
-// O(V 3^T + V^2 2^T)
 struct SteinerTree { // 0-base
-  static const int T = 10, N = 105, INF = 1e9;
   int n, dst[N][N], dp[1 << T][N], tdst[N];
-  int vcost[N]; // the cost of vertexs
+  int vcst[N]; // the cost of vertexs
   void init(int _n) {
     n = _n;
     for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) dst[i][j] = INF;
-      dst[i][i] = vcost[i] = 0;
+      fill_n(dst[i], n, INF);
+      dst[i][i] = vcst[i] = 0;
     }
   }
+  void chmin(int &x, int val) {
+    x = min(x, val);
+  }
   void add_edge(int ui, int vi, int wi) {
-    dst[ui][vi] = min(dst[ui][vi], wi);
+    chmin(dst[ui][vi], wi);
   }
   void shortest_path() {
     for (int k = 0; k < n; ++k)
       for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-          dst[i][j] =
-            min(dst[i][j], dst[i][k] + dst[k][j]);
+          chmin(dst[i][j], dst[i][k] + dst[k][j]);
   }
-  int solve(const vector<int> &ter) {
+  int solve(const vector<int>& ter) {
     shortest_path();
-    int t = SZ(ter);
-    for (int i = 0; i < (1 << t); ++i)
-      for (int j = 0; j < n; ++j) dp[i][j] = INF;
-    for (int i = 0; i < n; ++i) dp[0][i] = vcost[i];
-    for (int msk = 1; msk < (1 << t); ++msk) {
+    int t = SZ(ter), full = (1 << t) - 1;
+    for (int i = 0; i <= full; ++i)
+      fill_n(dp[i], n, INF);
+    copy_n(vcst, n, dp[0]);
+    for (int msk = 1; msk <= full; ++msk) {
       if (!(msk & (msk - 1))) {
         int who = __lg(msk);
         for (int i = 0; i < n; ++i)
-          dp[msk][i] =
-            vcost[ter[who]] + dst[ter[who]][i];
+          dp[msk][i] = vcst[ter[who]] + dst[ter[who]][i];
       }
       for (int i = 0; i < n; ++i)
-        for (int submsk = (msk - 1) & msk; submsk;
-             submsk = (submsk - 1) & msk)
-          dp[msk][i] = min(dp[msk][i],
-            dp[submsk][i] + dp[msk ^ submsk][i] -
-              vcost[i]);
+        for (int sub = (msk - 1) & msk; sub; sub = (sub - 1) & msk)
+          chmin(dp[msk][i], dp[sub][i] + dp[msk ^ sub][i] - vcst[i]);
       for (int i = 0; i < n; ++i) {
         tdst[i] = INF;
         for (int j = 0; j < n; ++j)
-          tdst[i] =
-            min(tdst[i], dp[msk][j] + dst[j][i]);
+          chmin(tdst[i], dp[msk][j] + dst[j][i]);
       }
-      for (int i = 0; i < n; ++i) dp[msk][i] = tdst[i];
+      copy_n(tdst, n, dp[msk]);
     }
-    int ans = INF;
-    for (int i = 0; i < n; ++i)
-      ans = min(ans, dp[(1 << t) - 1][i]);
-    return ans;
+    return *min_element(dp[full], dp[full] + n);
   }
-};
+}; // O(V 3^T + V^2 2^T)
