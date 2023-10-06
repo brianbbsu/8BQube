c58f235e5cd5f22249db791a093d6bbdd5e7ac0f
baluteshih <brian369999@gmail.com>
2023-10-07 03:07:05 +0800

    Update Maximum_Clique_Dyn.cpp, remove Suffix Array from codebook

--- codebook/2_Graph/Maximum_Clique_Dyn.cpp
+++ codebook/2_Graph/Maximum_Clique_Dyn.cpp
@@ -1,64 +1,50 @@
-const int N = 150;
-struct MaxClique { // Maximum Clique
-  bitset<N> a[N], cs[N];
+struct MaxClique { // fast when N <= 100
+  bitset<N> G[N], cs[N];
   int ans, sol[N], q, cur[N], d[N], n;
   void init(int _n) {
     n = _n;
-    for (int i = 0; i < n; i++) a[i].reset();
+    for (int i = 0; i < n; ++i) G[i].reset();
   }
-  void addEdge(int u, int v) { a[u][v] = a[v][u] = 1; }
-  void csort(vector<int> &r, vector<int> &c) {
-    int mx = 1, km = max(ans - q + 1, 1), t = 0,
-        m = r.size();
-    cs[1].reset(), cs[2].reset();
-    for (int i = 0; i < m; i++) {
-      int p = r[i], k = 1;
-      while ((cs[k] & a[p]).count()) k++;
-      if (k > mx) mx++, cs[mx + 1].reset();
-      cs[k][p] = 1;
-      if (k < km) r[t++] = p;
+  void add_edge(int u, int v) { 
+    G[u][v] = G[v][u] = 1;
+  }
+  void pre_dfs(vector<int> &r, int l, bitset<N> mask) {
+    if (l < 4) {
+      for (int i : r) d[i] = (G[i] & mask).count();
+      sort(ALL(r), [&](int x, int y) { return d[x] > d[y]; });
     }
-    c.resize(m);
-    if (t) c[t - 1] = 0;
-    for (int k = km; k <= mx; k++)
-      for (int p = cs[k]._Find_first(); p < N;
-           p = cs[k]._Find_next(p))
-        r[t] = p, c[t] = k, t++;
+    vector<int> c(SZ(r));
+    int lft = max(ans - q + 1, 1), rgt = 1, tp = 0;
+    cs[1].reset(), cs[2].reset();
+    for (int p : r) {
+      int k = 1;
+      while ((cs[k] & G[p]).any()) ++k;
+      if (k > rgt) cs[++rgt + 1].reset();
+      cs[k][p] = 1;
+      if (k < lft) r[tp++] = p;
+    }
+    for (int k = lft; k <= rgt; ++k)
+      for (int p = cs[k]._Find_first(); p < N; p = cs[k]._Find_next(p))
+        r[tp] = p, c[tp] = k, ++tp; 
+    dfs(r, c, l + 1, mask);
   }
-  void dfs(vector<int> &r, vector<int> &c, int l,
-    bitset<N> mask) {
+  void dfs(vector<int> &r, vector<int> &c, int l, bitset<N> mask) {
     while (!r.empty()) {
       int p = r.back();
       r.pop_back(), mask[p] = 0;
       if (q + c.back() <= ans) return;
       cur[q++] = p;
-      vector<int> nr, nc;
-      bitset<N> nmask = mask & a[p];
-      for (int i : r)
-        if (a[p][i]) nr.push_back(i);
-      if (!nr.empty()) {
-        if (l < 4) {
-          for (int i : nr)
-            d[i] = (a[i] & nmask).count();
-          sort(nr.begin(), nr.end(),
-            [&](int x, int y) { return d[x] > d[y]; });
-        }
-        csort(nr, nc), dfs(nr, nc, l + 1, nmask);
-      } else if (q > ans) ans = q, copy_n(cur, q, sol);
-      c.pop_back(), q--;
+      vector<int> nr;
+      for (int i : r) if (G[p][i]) nr.pb(i);
+      if (!nr.empty()) pre_dfs(nr, l, mask & G[p]); 
+      else if (q > ans) ans = q, copy_n(cur, q, sol);
+      c.pop_back(), --q;
     }
   }
-  int solve(bitset<N> mask = bitset<N>(
-              string(N, '1'))) { // vertex mask
-    vector<int> r, c;
-    ans = q = 0;
-    for (int i = 0; i < n; i++)
-      if (mask[i]) r.push_back(i);
-    for (int i = 0; i < n; i++)
-      d[i] = (a[i] & mask).count();
-    sort(r.begin(), r.end(),
-      [&](int i, int j) { return d[i] > d[j]; });
-    csort(r, c), dfs(r, c, 1, mask);
-    return ans; // sol[0 ~ ans-1]
+  int solve() {
+    vector<int> r(n);
+    ans = q = 0, iota(ALL(r), 0);
+    pre_dfs(r, 0, bitset<N>(string(n, '1')));
+    return ans;
   }
-} graph;
+};
