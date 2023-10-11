de420e5691c7e9e93ff9336ff23e3e1fc62cf1d2
baluteshih <brian369999@gmail.com>
2023-10-11 14:34:57 +0800

    fix the indent of TangentPointToHull.cpp

--- codebook/8_Geometry/TangentPointToHull.cpp
+++ codebook/8_Geometry/TangentPointToHull.cpp
@@ -2,8 +2,8 @@
   return arbitrary point on the tangent line */
 pii get_tangent(vector<pll> &C, pll p) {
   auto gao = [&](int s) {
-    return cyc_tsearch(SZ(C), 
-    [&](int x, int y) { return ori(p, C[x], C[y]) == s; });
+    return cyc_tsearch(SZ(C), [&](int x, int y) 
+    { return ori(p, C[x], C[y]) == s; });
   };
   return pii(gao(1), gao(-1));
 } // return (a, b), ori(p, C[a], C[b]) >= 0
