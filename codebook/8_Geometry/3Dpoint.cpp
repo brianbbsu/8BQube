05c90c6003fedff6ad67325992fe1e2751cf75c1
baluteshih <brian369999@gmail.com>
2023-10-14 01:22:38 +0800

    Update 3Dpoint.cpp

--- codebook/8_Geometry/3Dpoint.cpp
+++ codebook/8_Geometry/3Dpoint.cpp
@@ -3,24 +3,30 @@ struct Point {
   Point(double _x = 0, double _y = 0, double _z = 0): x(_x), y(_y), z(_z){}
   Point(pdd p) { x = p.X, y = p.Y, z = abs2(p); }
 };
-Point operator-(const Point &p1, const Point &p2)
+Point operator-(Point p1, Point p2)
 { return Point(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z); }
-Point operator+(const Point &p1, const Point &p2)
+Point operator+(Point p1, Point p2)
 { return Point(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z); }
-Point operator/(const Point &p1, const double &v)
+Point operator*(Point p1, double v)
+{ return Point(p1.x * v, p1.y * v, p1.z * v); }
+Point operator/(Point p1, double v)
 { return Point(p1.x / v, p1.y / v, p1.z / v); }
-Point cross(const Point &p1, const Point &p2)
+Point cross(Point p1, Point p2)
 { return Point(p1.y * p2.z - p1.z * p2.y, p1.z * p2.x - p1.x * p2.z, p1.x * p2.y - p1.y * p2.x); }
-double dot(const Point &p1, const Point &p2)
+double dot(Point p1, Point p2)
 { return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z; }
-double abs(const Point &a)
+double abs(Point a)
 { return sqrt(dot(a, a)); }
-Point cross3(const Point &a, const Point &b, const Point &c)
+Point cross3(Point a, Point b, Point c)
 { return cross(b - a, c - a); }
 double area(Point a, Point b, Point c)
 { return abs(cross3(a, b, c)); }
 double volume(Point a, Point b, Point c, Point d)
 { return dot(cross3(a, b, c), d - a); }
+//Azimuthal angle (longitude) to x-axis in interval [-pi, pi]
+double phi(Point p) { return atan2(p.y, p.x); } 
+//Zenith angle (latitude) to the z-axis in interval [0, pi]
+double theta(Point p) { return atan2(sqrt(p.x * p.x + p.y * p.y), p.z); }
 Point masscenter(Point a, Point b, Point c, Point d)
 { return (a + b + c + d) / 4; }
 pdd proj(Point a, Point b, Point c, Point u) {
@@ -32,4 +38,9 @@ pdd proj(Point a, Point b, Point c, Point u) {
   e2 = e2 / abs(e2);
   Point p = u - a;
   return pdd(dot(p, e1), dot(p, e2));
+}
+Point rotate_around(Point p, double angle, Point axis) {
+  double s = sin(angle), c = cos(angle);
+  Point u = axis / abs(axis);
+  return u * dot(u, p) * (1 - c) + p * c + cross(u, p) * s;
 }
