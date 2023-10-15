7a48d9e707632d5da140ac0f503797652afe8761
i1011 <56438495+i1011@users.noreply.github.com>
2023-10-15 20:12:22 +0800

    Update DLX.cpp

--- codebook/9_Else/DLX.cpp
+++ codebook/9_Else/DLX.cpp
@@ -1,13 +1,14 @@
 #define TRAV(i, link, start) for (int i = link[start]; i != start; i = link[i])
-template<bool A, bool B = !A> // A: Exact
+template<bool E> // E: Exact, NN: num of 1s, RR: num of rows
 struct DLX {
-  int lt[NN], rg[NN], up[NN], dn[NN], cl[NN], rw[NN], bt[NN], s[NN], head, sz, ans;
-  int columns;
+  int lt[NN], rg[NN], up[NN], dn[NN], rw[NN], cl[NN], bt[NN], s[NN], head, sz, ans;
+  int rows, columns;
   bool vis[NN];
+  bitset<RR> sol, cur; // not sure
   void remove(int c) {
-    if (A) lt[rg[c]] = lt[c], rg[lt[c]] = rg[c];
+    if (E) lt[rg[c]] = lt[c], rg[lt[c]] = rg[c];
     TRAV(i, dn, c) {
-      if (A) {
+      if (E) {
         TRAV(j, rg, i)
           up[dn[j]] = up[j], dn[up[j]] = dn[j], --s[cl[j]];
       } else {
@@ -17,17 +18,17 @@ struct DLX {
   }
   void restore(int c) {
     TRAV(i, up, c) {
-      if (A) {
+      if (E) {
         TRAV(j, lt, i)
           ++s[cl[j]], up[dn[j]] = j, dn[up[j]] = j;
       } else {
         lt[rg[i]] = rg[lt[i]] = i;
       }
     }
-    if (A) lt[rg[c]] = c, rg[lt[c]] = c;
+    if (E) lt[rg[c]] = c, rg[lt[c]] = c;
   }
   void init(int c) {
-    columns = c;
+    rows = 0, columns = c;
     for (int i = 0; i < c; ++i) {
       up[i] = dn[i] = bt[i] = i;
       lt[i] = i == 0 ? c : i - 1;
@@ -38,7 +39,7 @@ struct DLX {
     up[c] = dn[c] = -1;
     head = c, sz = c + 1;
   }
-  void insert(int r, const vector<int> &col) {
+  void insert(const vector<int> &col) {
     if (col.empty()) return;
     int f = sz;
     for (int i = 0; i < (int)col.size(); ++i) {
@@ -46,15 +47,15 @@ struct DLX {
       dn[bt[c]] = v;
       up[v] = bt[c], bt[c] = v;
       rg[v] = (i + 1 == (int)col.size() ? f : v + 1);
-      rw[v] = r, cl[v] = c;
+      rw[v] = rows, cl[v] = c;
       ++s[c];
       if (i > 0) lt[v] = v - 1;
     }
-    lt[f] = sz - 1;
+    ++rows, lt[f] = sz - 1;
   }
   int h() {
     int ret = 0;
-    memset(vis, 0, sizeof(bool) * sz);
+    fill_n(vis, sz, false);
     TRAV(x, rg, head) {
       if (vis[x]) continue;
       vis[x] = true, ++ret;
@@ -63,25 +64,25 @@ struct DLX {
     return ret;
   }
   void dfs(int dep) {
-    if (dep + (A ? 0 : h()) >= ans) return;
-    if (rg[head] == head) return ans = dep, void();
+    if (dep + (E ? 0 : h()) >= ans) return;
+    if (rg[head] == head) return sol = cur, ans = dep, void();
     if (dn[rg[head]] == rg[head]) return;
     int w = rg[head];
     TRAV(x, rg, head) if (s[x] < s[w]) w = x;
-    if (A) remove(w);
+    if (E) remove(w);
     TRAV(i, dn, w) {
-      if (B) remove(i);
-      TRAV(j, rg, i) remove(A ? cl[j] : j);
-      dfs(dep + 1);
-      TRAV(j, lt, i) restore(A ? cl[j] : j);
-      if (B) restore(i);
+      if (!E) remove(i);
+      TRAV(j, rg, i) remove(E ? cl[j] : j);
+      cur.set(rw[i]), dfs(dep + 1), cur.reset(rw[i]);
+      TRAV(j, lt, i) restore(E ? cl[j] : j);
+      if (!E) restore(i);
     }
-    if (A) restore(w);
+    if (E) restore(w);
   }
   int solve() {
     for (int i = 0; i < columns; ++i)
       dn[bt[i]] = i, up[i] = bt[i];
-    ans = 1e9, dfs(0);
+    ans = 1e9, sol.reset(), dfs(0);
     return ans;
   }
 };
