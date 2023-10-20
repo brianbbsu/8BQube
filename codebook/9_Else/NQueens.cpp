18729acf59015a3e0f496226dbec64ca79fc98bb
baluteshih <brian369999@gmail.com>
2023-10-21 02:10:00 +0800

    NQueens.cpp, push_back -> pb

--- codebook/9_Else/NQueens.cpp
+++ codebook/9_Else/NQueens.cpp
@@ -1,16 +1,16 @@
 void solve(vector<int> &ret, int n) { // no sol when n=2,3
   if (n % 6 == 2) {
-    for (int i = 2; i <= n; i += 2) ret.push_back(i);
-    ret.push_back(3); ret.push_back(1);
-    for (int i = 7; i <= n; i += 2) ret.push_back(i);
-    ret.push_back(5);
+    for (int i = 2; i <= n; i += 2) ret.pb(i);
+    ret.pb(3); ret.pb(1);
+    for (int i = 7; i <= n; i += 2) ret.pb(i);
+    ret.pb(5);
   } else if (n % 6 == 3) {
-    for (int i = 4; i <= n; i += 2) ret.push_back(i);
-    ret.push_back(2);
-    for (int i = 5; i <= n; i += 2) ret.push_back(i);
-    ret.push_back(1); ret.push_back(3);
+    for (int i = 4; i <= n; i += 2) ret.pb(i);
+    ret.pb(2);
+    for (int i = 5; i <= n; i += 2) ret.pb(i);
+    ret.pb(1); ret.pb(3);
   } else {
-    for (int i = 2; i <= n; i += 2) ret.push_back(i);
-    for (int i = 1; i <= n; i += 2) ret.push_back(i);
+    for (int i = 2; i <= n; i += 2) ret.pb(i);
+    for (int i = 1; i <= n; i += 2) ret.pb(i);
   }
 }
