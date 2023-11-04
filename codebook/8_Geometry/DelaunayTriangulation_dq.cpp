7d3da09f474174cd95ee3a18b9b0759275f1bfe1
baluteshih <brian369999@gmail.com>
2023-11-04 14:59:15 +0800

    Update DelaunayTriangulation_dq.cpp

--- codebook/8_Geometry/DelaunayTriangulation_dq.cpp
+++ codebook/8_Geometry/DelaunayTriangulation_dq.cpp
@@ -28,55 +28,32 @@ struct Delaunay { // 0-base
   void divide(int l, int r) {
     if (l == r) return;
     if (l + 1 == r) return addEdge(l, l + 1);
-    int mid = (l + r) >> 1;
+    int mid = (l + r) >> 1, nw[2] = {l, r};
     divide(l, mid), divide(mid + 1, r);
-    int nowl = l, nowr = r;
-    for (int update = 1; update;) {
-      update = 0;
-      pll ptL = p[nowl], ptR = p[nowr];
-      for (auto it : head[nowl]) {
-        pll t = p[it.id];
-        int v = ori(ptR, ptL, t);
-        if (v > 0 || (v == 0 && abs2(ptR - t) < abs2(ptR - ptL))) {
-          nowl = it.id, update = 1;
-          break;
-        }
+    auto gao = [&](int t) {
+      pll pt[2] = {p[nw[0]], p[nw[1]]};
+      for (auto it : head[nw[t]]) {
+        int v = ori(pt[1], pt[0], p[it.id]);
+        if (v > 0 || (v == 0 && abs2(pt[t ^ 1] - p[it.id]) < abs2(pt[1] - pt[0])))
+          return nw[t] = it.id, true;
       }
-      if (update)	continue;
-      for (auto it : head[nowr]) {
-        pll t = p[it.id];
-        int v = ori(ptL, ptR, t);
-        if (v < 0 || (v == 0 && abs2(ptL - t) < abs2(ptL - ptR))) {
-          nowr = it.id, update = 1;
-          break;
-        }
-      }
-    }
-    addEdge(nowl, nowr); // add tangent
+      return false;
+    };
+    while (gao(0) || gao(1));
+    addEdge(nw[0], nw[1]); // add tangent
     while (true) {
-      pll ptL = p[nowl], ptR = p[nowr];
-      int ch = -1, side = 0;
-      for (auto it : head[nowl])
-        if (ori(ptL, ptR, p[it.id]) > 0 && (ch == -1 || in_cc({ptL, ptR, p[ch]}, p[it.id])))
-          ch = it.id, side = -1;
-      for (auto it : head[nowr])
-        if (ori(ptR, p[it.id], ptL) > 0 && (ch == -1 || in_cc({ptL, ptR, p[ch]}, p[it.id])))
-          ch = it.id, side = 1;
+      pll pt[2] = {p[nw[0]], p[nw[1]]};
+      int ch = -1, sd = 0;
+      for (int t = 0; t < 2; ++t)
+          for (auto it : head[nw[t]])
+              if (ori(pt[0], pt[1], p[it.id]) > 0 && (ch == -1 || in_cc({pt[0], pt[1], p[ch]}, p[it.id])))
+                  ch = it.id, sd = t;
       if (ch == -1) break; // upper common tangent
-      if (side == -1) {
-        for (auto it = head[nowl].begin(); it != head[nowl].end(); )
-          if (seg_strict_intersect(ptL, p[it->id], ptR, p[ch]))
-            head[it->id].erase(it->twin), head[nowl].erase(it++);
-          else ++it;
-        nowl = ch, addEdge(nowl, nowr);
-      } 
-      else {
-        for (auto it = head[nowr].begin(); it != head[nowr].end(); )
-          if (seg_strict_intersect(ptR, p[it->id], ptL, p[ch]))
-            head[it->id].erase(it->twin), head[nowr].erase(it++);
-          else ++it;
-        nowr = ch, addEdge(nowl, nowr);
-      }
+      for (auto it = head[nw[sd]].begin(); it != head[nw[sd]].end(); )
+        if (seg_strict_intersect(pt[sd], p[it->id], pt[sd ^ 1], p[ch]))
+          head[it->id].erase(it->twin), head[nw[sd]].erase(it++);
+        else ++it;
+      nw[sd] = ch, addEdge(nw[0], nw[1]);
     }
   }
 } tool;
