e7d5f1ed9efa096c53c481de0c52ed4fd527b8b5
baluteshih <brian369999@gmail.com>
2024-02-19 22:32:30 +0800

    Update Aho-Corasick_Automatan.cpp

--- codebook/5_String/Aho-Corasick_Automatan.cpp
+++ codebook/5_String/Aho-Corasick_Automatan.cpp
@@ -1,42 +1,36 @@
-const int len = 400000, sigma = 26;
 struct AC_Automatan {
-  int nx[len][sigma], fl[len], cnt[len], pri[len], top;
+  int nx[len][sigma], fl[len], cnt[len], ord[len], top;
+  int rnx[len][sigma]; // node actually be reached
   int newnode() {
-    fill(nx[top], nx[top] + sigma, -1);
+    fill_n(nx[top], sigma, -1);
     return top++;
   }
   void init() { top = 1, newnode(); }
-  int input(
-    string &s) { // return the end_node of string
+  int input(string &s) {
     int X = 1;
     for (char c : s) {
-      if (!~nx[X][c - 'a']) nx[X][c - 'a'] = newnode();
-      X = nx[X][c - 'a'];
+      if (!~nx[X][c - 'A']) nx[X][c - 'A'] = newnode();
+      X = nx[X][c - 'A'];
     }
-    return X;
+    return X; // return the end node of string
   }
   void make_fl() {
     queue<int> q;
     q.push(1), fl[1] = 0;
-    for (int t = 0; !q.empty();) {
+    for (int t = 0; !q.empty(); ) {
       int R = q.front();
-      q.pop(), pri[t++] = R;
+      q.pop(), ord[t++] = R;
       for (int i = 0; i < sigma; ++i)
         if (~nx[R][i]) {
-          int X = nx[R][i], Z = fl[R];
-          for (; Z && !~nx[Z][i];) Z = fl[Z];
+          int X = rnx[R][i] = nx[R][i], Z = fl[R];
+          for (; Z && !~nx[Z][i]; ) Z = fl[Z];
           fl[X] = Z ? nx[Z][i] : 1, q.push(X);
         }
+        else rnx[R][i] = R > 1 ? rnx[fl[R]][i] : 1;
     }
   }
-  void get_v(string &s) {
-    int X = 1;
-    fill(cnt, cnt + top, 0);
-    for (char c : s) {
-      while (X && !~nx[X][c - 'a']) X = fl[X];
-      X = X ? nx[X][c - 'a'] : 1, ++cnt[X];
-    }
+  void solve() {
     for (int i = top - 2; i > 0; --i)
-      cnt[fl[pri[i]]] += cnt[pri[i]];
+      cnt[fl[ord[i]]] += cnt[ord[i]];
   }
-};
+} ac;
