9ac9e1972fc5727ee84cb77995b7e8c140e09cfd
baluteshih <brian369999@gmail.com>
2024-04-05 23:20:58 +0800

    Update Heavy_light_Decomposition.cpp

--- codebook/3_Data_Structure/Heavy_light_Decomposition.cpp
+++ codebook/3_Data_Structure/Heavy_light_Decomposition.cpp
@@ -1,43 +1,40 @@
 struct Heavy_light_Decomposition { // 1-base
   int n, ulink[N], deep[N], mxson[N], w[N], pa[N];
-  int t, pl[N], data[N], dt[N], bln[N], edge[N], et;
-  vector<pii> G[N];
+  int t, pl[N], data[N], val[N]; // val: vertex data
+  vector<int> G[N];
   void init(int _n) {
-    n = _n, t = 0, et = 1;
+    n = _n;
     for (int i = 1; i <= n; ++i)
       G[i].clear(), mxson[i] = 0;
   }
-  void add_edge(int a, int b, int w) {
-    G[a].pb(pii(b, et));
-    G[b].pb(pii(a, et));
-    edge[et++] = w;
+  void add_edge(int a, int b) {
+    G[a].pb(b), G[b].pb(a);
   }
   void dfs(int u, int f, int d) {
     w[u] = 1, pa[u] = f, deep[u] = d++;
-    for (auto &i : G[u])
-      if (i.X != f) {
-        dfs(i.X, u, d), w[u] += w[i.X];
-        if (w[mxson[u]] < w[i.X]) mxson[u] = i.X;
-      } else bln[i.Y] = u, dt[u] = edge[i.Y];
+    for (int &i : G[u])
+      if (i != f) {
+        dfs(i, u, d), w[u] += w[i];
+        if (w[mxson[u]] < w[i]) mxson[u] = i;
+      }
   }
   void cut(int u, int link) {
-    data[pl[u] = t++] = dt[u], ulink[u] = link;
+    data[pl[u] = ++t] = val[u], ulink[u] = link;
     if (!mxson[u]) return;
     cut(mxson[u], link);
-    for (auto i : G[u])
-      if (i.X != pa[u] && i.X != mxson[u])
-        cut(i.X, i.X);
+    for (int i : G[u])
+      if (i != pa[u] && i != mxson[u])
+        cut(i, i);
   }
   void build() { dfs(1, 1, 1), cut(1, 1), /*build*/; }
   int query(int a, int b) {
-    int ta = ulink[a], tb = ulink[b], re = 0;
-    while (ta != tb)
-      if (deep[ta] < deep[tb])
-        /*query*/, tb = ulink[b = pa[tb]];
-      else /*query*/, ta = ulink[a = pa[ta]];
-    if (a == b) return re;
+    int ta = ulink[a], tb = ulink[b], res = 0;
+    while (ta != tb) {
+      if (deep[ta] > deep[tb]) swap(ta, tb), swap(a, b);
+      // query(pl[tb], pl[b])
+      tb = ulink[b = pa[tb]];
+    }
     if (pl[a] > pl[b]) swap(a, b);
-    /*query*/
-    return re;
+    // query(pl[a], pl[b])
   }
 };
